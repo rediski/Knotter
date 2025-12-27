61a6ebc4103d550122c5b77e2e4c32ea9cec197a
Attato <rde8519@mail.ru>
2025-12-27 18:07:54 +0300

    delete onClickOutside, replace placeholder with label, and add a null value button.

--- app/components/UI/Select.tsx
+++ app/components/UI/Select.tsx
@@ -1,69 +1,80 @@
 'use client';
 
-import { memo, useState, useRef, useEffect } from 'react';
-import { ChevronDown } from 'lucide-react';
+import { memo, useState, useRef } from 'react';
+import { ChevronDown, Ban } from 'lucide-react';
 
 interface SelectProps {
     value: string | null;
     options: string[];
     onChange: (value: string | null) => void;
-    className?: string;
-    placeholder?: string;
+    label?: string;
 }
 
-export const Select = memo(function Select({ value, options, onChange, className = '', placeholder = '-' }: SelectProps) {
+export const Select = memo(function Select({ value, options, onChange, label }: SelectProps) {
     const [isOpen, setIsOpen] = useState(false);
     const selectRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const onClickOutside = (event: MouseEvent) => {
-            if (selectRef.current && !selectRef.current.contains(event.target as Node)) {
-                setIsOpen(false);
-            }
-        };
-
-        document.addEventListener('mousedown', onClickOutside);
-
-        return () => document.removeEventListener('mousedown', onClickOutside);
-    }, []);
-
     const selected = options.find((option) => option === value);
 
+    console.log(selected);
     return (
-        <div ref={selectRef} className="relative w-full">
+        <div ref={selectRef} className="flex flex-col w-full">
             <button
                 type="button"
                 onClick={() => setIsOpen((v) => !v)}
                 className={`
-                    flex items-center justify-between h-8 px-3 w-full rounded-md bg-depth-1 hover:bg-depth-2 cursor-pointer
-                    ${className}
+                    flex items-center gap-2 h-8 px-3 w-full cursor-pointer
+                    ${isOpen ? 'rounded-t-md bg-depth-3' : 'rounded-md bg-depth-2 hover:bg-depth-3'}
                 `}
             >
-                <span className="truncate">{selected ?? '-'}</span>
-                <ChevronDown size={14} />
+                <ChevronDown size={14} className={`${isOpen && 'rotate-180'} transition-transform`} />
+                <span className="truncate">{selected ?? label}</span>
             </button>
 
             {isOpen && (
-                <div className="absolute flex flex-col gap-1 mt-1 w-full rounded-md border border-depth-3 bg-depth-1 shadow-lg p-1 z-10">
-                    {options.map((option) => {
-                        const isSelected = option === value;
+                <div className="w-full rounded-b-md bg-depth-2">
+                    {label && (
+                        <div className="m-1 p-1 text-gray border-b border-depth-4 text-xs tracking-wide text-muted-foreground select-none">
+                            {label}
+                        </div>
+                    )}
 
-                        return (
+                    <div className="flex flex-col gap-1 p-1">
+                        {options.map((option) => {
+                            const isSelected = option === value;
+
+                            return (
+                                <button
+                                    key={option}
+                                    type="button"
+                                    onClick={() => {
+                                        onChange(option);
+                                    }}
+                                    className={`
+                                        w-full px-3 h-8 text-left text-sm truncate rounded-md cursor-pointer
+                                        ${isSelected ? 'bg-bg-accent/10 text-text-accent' : 'bg-depth-3 hover:bg-depth-4'}
+                                    `}
+                                >
+                                    {option}
+                                </button>
+                            );
+                        })}
+
+                        {selected !== undefined && <hr className="border-b-0 border-depth-4 my-0.5" />}
+
+                        {selected !== undefined && (
                             <button
-                                key={option}
                                 type="button"
                                 onClick={() => {
-                                    onChange(option);
-                                    setIsOpen(false);
+                                    onChange(null);
                                 }}
-                                className={`w-full px-3 py-2 text-left text-sm truncate rounded-md cursor-pointer
-                                    ${isSelected ? 'bg-bg-accent text-white' : 'hover:bg-depth-2'}
-                                `}
+                                className="flex items-center justify-between w-full px-3 h-8 text-left text-sm text-red truncate rounded-md cursor-pointer bg-depth-3 hover:bg-depth-4"
                             >
-                                {option}
+                                Не выбрано
+                                <Ban size={14} />
                             </button>
-                        );
-                    })}
+                        )}
+                    </div>
                 </div>
             )}
         </div>
