f418f8e483096cb1f9fa03e88dabca37a1bb1412
Attato <rde8519@mail.ru>
2025-12-28 20:02:42 +0300

    change handleDeleteItems to deleteItems

--- app/canvas/utils/items/deleteItems.ts
+++ app/canvas/utils/items/deleteItems.ts
@@ -1,13 +1,16 @@
 import { CanvasItem } from '@/canvas/canvas.types';
 import { getNodes } from '@/canvas/utils/nodes/getNodes';
 import { getEdges } from '@/canvas/utils/edges/getEdges';
+import { getTexts } from '@/canvas/utils/texts/getTexts';
 
-export function handleDeleteItems(items: CanvasItem[], selectedIds: string[]): CanvasItem[] {
+export function deleteItems(items: CanvasItem[], selectedIds: string[]): CanvasItem[] {
     const nodes = getNodes(items);
     const edges = getEdges(items);
+    const texts = getTexts(items);
 
     const toDeleteNodes = new Set(nodes.filter((n) => selectedIds.includes(n.id)).map((n) => n.id));
     const toDeleteEdges = new Set(edges.filter((e) => selectedIds.includes(e.id)).map((e) => e.id));
+    const toDeleteTexts = new Set(texts.filter((t) => selectedIds.includes(t.id)).map((t) => t.id));
 
     return items.filter((item) => {
         if (item.kind === 'node') {
@@ -16,6 +19,10 @@ export function handleDeleteItems(items: CanvasItem[], selectedIds: string[]): C
 
         if (item.kind === 'edge') {
             return !toDeleteEdges.has(item.id) && !toDeleteNodes.has(item.from) && !toDeleteNodes.has(item.to);
+        }
+
+        if (item.kind === 'text') {
+            return !toDeleteTexts.has(item.id);
         }
 
         return true;
