61e789cec1cf81c76c9d9de26d9c1578f85c29a0
Attato <rde8519@mail.ru>
2025-12-28 20:02:06 +0300

    change handleAddItem to createItem

---
+++ app/canvas/utils/items/createItem.ts
@@ -0,0 +1,28 @@
+import type { CanvasItem } from '@/canvas/canvas.types';
+import type { CreateItemParams } from './createItem.types';
+
+import { MAX_CANVAS_ITEMS } from '@/canvas/canvas.constants';
+import { createNode } from '../nodes/createNode';
+import { createEdge } from '../edges/createEdge';
+import { createText } from '../texts/createText';
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+
+export function createItem(params: CreateItemParams): CanvasItem | null {
+    const { items } = useCanvasStore.getState();
+
+    if (items.length >= MAX_CANVAS_ITEMS) {
+        console.warn(`Достигнут лимит элементов на канвасе (${MAX_CANVAS_ITEMS}). Новый элемент добавлен не будет.`);
+        return null;
+    }
+
+    switch (params.type) {
+        case 'node':
+            return createNode(params.state.nodes, params.position);
+
+        case 'edge':
+            return createEdge(params.state.edges, params.fromNode, params.toNode);
+
+        case 'text':
+            return createText(params.state.texts, params.content, params.position);
+    }
+}
