556c5b2def2d7cc3d77798b24a292193d71bb402
Attato <rde8519@mail.ru>
2026-01-04 22:17:52 +0300

    change position to fixed

--- app/components/UI/ContextMenu.tsx
+++ app/components/UI/ContextMenu.tsx
@@ -14,7 +14,7 @@ export const ContextMenu = memo(
         return (
             <div
                 ref={ref}
-                className="min-w-56 absolute bg-depth-2 border border-depth-6 text-foreground rounded shadow-md py-1 text-sm z-50"
+                className="min-w-56 fixed bg-depth-2 border border-depth-6 text-foreground rounded shadow-md py-1 text-sm z-50"
                 style={{ top: position.y, left: position.x }}
             >
                 {children}
