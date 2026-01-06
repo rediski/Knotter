fe4d7af172f2e0556ba57fb1a87c6bfce0a08b84
Attato <rde8519@mail.ru>
2026-01-06 23:01:39 +0300

    implemented a working version of SidebarPanels

--- app/canvas/components/CanvasSidebar/SidebarPanels.tsx
+++ app/canvas/components/CanvasSidebar/SidebarPanels.tsx
@@ -1,16 +1,61 @@
 'use client';
 
+import { SidebarPanel } from '@/canvas/components/CanvasSidebar/SidebarPanel';
+
 import { useCanvasStore } from '@/canvas/store/canvasStore';
-import type { Tab } from '@/canvas/components/CanvasSidebar/CanvasSidebar';
 
-interface SidebarPanelsProps {
-    tabs: Tab[];
-}
+import { useSidebarPanels } from '@/canvas/hooks/useSideBarPanels';
+import { EmptyState } from '@/components/UI/EmptyState';
 
-export function SidebarPanels({ tabs }: SidebarPanelsProps) {
-    const activeTab = useCanvasStore((s) => s.activeTab);
+export function SidebarPanels() {
+    const panels = useCanvasStore((state) => state.sidebarPanels);
+    const { panelHeights, startPanelResize, resizingPanelIndex } = useSidebarPanels();
 
-    const panel = tabs.find((t) => t.id === activeTab)?.content;
+    const getPanelTop = (index: number) => panelHeights.slice(0, index).reduce((sum, height) => sum + height, 0);
 
-    return <div className="flex-1 overflow-auto bg-depth-1 border-r border-depth-3">{panel}</div>;
+    return (
+        <div
+            className={`
+                relative flex-1 
+                ${resizingPanelIndex !== null ? 'select-none' : ''}
+            `}
+        >
+            {panels.length > 0 ? (
+                panels.map((panel, index) => {
+                    const isNotLastPanel = index < panels.length - 1;
+
+                    return (
+                        <div
+                            key={panel.id}
+                            className="absolute left-0 right-0 flex flex-col bg-depth-1 border-t border-depth-5 first:border-t-0"
+                            style={{
+                                height: panelHeights[index],
+                                top: getPanelTop(index),
+                                zIndex: panels.length + index,
+                            }}
+                        >
+                            <SidebarPanel panel={panel} />
+
+                            {isNotLastPanel && (
+                                <div
+                                    onMouseDown={(e) => {
+                                        e.stopPropagation();
+                                        startPanelResize(index, e.clientY);
+                                    }}
+                                    onContextMenu={(e) => e.stopPropagation()}
+                                    className={`
+                                        absolute -bottom-1 left-0 right-0 h-2 cursor-row-resize hover:bg-depth-5 
+                                        ${resizingPanelIndex === index ? 'bg-depth-5' : 'bg-transparent'}
+                                    `}
+                                    style={{ zIndex: panels.length - index + 1 }}
+                                />
+                            )}
+                        </div>
+                    );
+                })
+            ) : (
+                <EmptyState message="Нажмите ПКМ по этой области, чтобы добавить панель" />
+            )}
+        </div>
+    );
 }
