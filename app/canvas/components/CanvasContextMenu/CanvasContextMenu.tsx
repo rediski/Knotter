3c36e194e5fcf58cd90cb8ab508a3bd39e2f7969
Attato <rde8519@mail.ru>
2026-01-06 22:53:25 +0300

    delete useContextMenuItems

--- app/canvas/components/CanvasContextMenu/CanvasContextMenu.tsx
+++ app/canvas/components/CanvasContextMenu/CanvasContextMenu.tsx
@@ -4,8 +4,13 @@ import { useEffect, useRef, memo } from 'react';
 import { ContextMenu } from '@/components/UI/ContextMenu';
 import { ContextMenuItem } from '@/components/UI/ContextMenuItem';
 
-import { MenuItem, useContextMenuItems } from '@/canvas/hooks/CanvasContextMenu/useContextMenuItems';
 import { useClickOutside } from '@/canvas/hooks/useClickOutside';
+import { useCanvasHandlers } from '@/canvas/hooks/useCanvasHandlers';
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+
+import { getNodes } from '@/canvas/utils/nodes/getNodes';
+import { getEdges } from '@/canvas/utils/edges/getEdges';
+import { getShape, getAllShapes } from '@/canvas/utils/nodes/getShape';
 
 type CanvasContextMenuProps = {
     isOpen: boolean;
@@ -15,7 +20,14 @@ type CanvasContextMenuProps = {
 
 export const CanvasContextMenu = memo(function CanvasContextMenu({ isOpen, position, closeMenu }: CanvasContextMenuProps) {
     const menuRef = useRef<HTMLDivElement | null>(null);
-    const { menuItems, offset } = useContextMenuItems();
+
+    const items = useCanvasStore((state) => state.items);
+    const selectedItemIds = useCanvasStore((state) => state.selectedItemIds);
+    const offset = useCanvasStore((state) => state.offset);
+
+    const nodes = getNodes(items);
+    const edges = getEdges(items);
+    const handlers = useCanvasHandlers();
 
     useClickOutside(menuRef, closeMenu);
 
@@ -25,31 +37,120 @@ export const CanvasContextMenu = memo(function CanvasContextMenu({ isOpen, posit
         }
     }, [offset.x, offset.y, closeMenu]);
 
-    const renderMenuItem = (item: MenuItem, close: () => void, key: number) => {
-        if (item.type === 'divider') {
-            return <hr key={key} className="border-b-0 border-depth-6 my-1" />;
-        }
-
-        return (
-            <ContextMenuItem
-                key={key}
-                onClick={() => {
-                    item.onClick?.();
-                    close();
-                }}
-                disabled={item.disabled}
-                shortcut={item.shortcut}
-                submenu={item.submenu?.map((sub, subIdx) => renderMenuItem(sub, close, subIdx))}
-                icon={item.icon}
-            >
-                {item.label}
-            </ContextMenuItem>
-        );
-    };
+    const onlyNodesSelected = selectedItemIds.every((id) => nodes.some((node) => node.id === id));
 
     return (
         <ContextMenu isOpen={isOpen} position={position} ref={menuRef}>
-            {menuItems.map((item, idx) => renderMenuItem(item, closeMenu, idx))}
+            <ContextMenuItem
+                submenu={[
+                    <ContextMenuItem
+                        key="select-all"
+                        onClick={() => {
+                            handlers.selectAll();
+                            closeMenu();
+                        }}
+                        disabled={items.length === 0}
+                        shortcut="Ctrl + A"
+                    >
+                        Выбрать всё
+                    </ContextMenuItem>,
+                    <ContextMenuItem
+                        key="select-all-nodes"
+                        onClick={() => {
+                            handlers.selectAllNodes();
+                            closeMenu();
+                        }}
+                        disabled={nodes.length === 0}
+                    >
+                        Выбрать все узлы
+                    </ContextMenuItem>,
+                    <ContextMenuItem
+                        key="select-all-edges"
+                        onClick={() => {
+                            handlers.selectAllEdges();
+                            closeMenu();
+                        }}
+                        disabled={edges.length === 0}
+                        shortcut="Ctrl + E"
+                    >
+                        Выбрать все связи
+                    </ContextMenuItem>,
+                ]}
+            >
+                Выбрать
+            </ContextMenuItem>
+
+            <ContextMenuItem
+                submenu={[
+                    <ContextMenuItem
+                        key="create-node"
+                        onClick={() => {
+                            handlers.addNode();
+                            closeMenu();
+                        }}
+                        shortcut="Shift + A"
+                    >
+                        Создать узел
+                    </ContextMenuItem>,
+                    <ContextMenuItem
+                        key="create-edge"
+                        onClick={() => {
+                            handlers.startEdge();
+                            closeMenu();
+                        }}
+                        disabled={selectedItemIds.length !== 1 || !nodes.some((n) => n.id === selectedItemIds[0])}
+                        shortcut="Shift + E"
+                    >
+                        Создать связь
+                    </ContextMenuItem>,
+                    <ContextMenuItem
+                        key="create-text"
+                        onClick={() => {
+                            handlers.addText();
+                            closeMenu();
+                        }}
+                        shortcut="Shift + T"
+                    >
+                        Создать текст
+                    </ContextMenuItem>,
+                ]}
+            >
+                Создать
+            </ContextMenuItem>
+
+            <ContextMenuItem
+                disabled={!onlyNodesSelected}
+                submenu={getAllShapes().map((type) => {
+                    const { label, icon } = getShape(type);
+                    return (
+                        <ContextMenuItem
+                            key={`shape-${type}`}
+                            onClick={() => {
+                                handlers.changeNodeShapeType(selectedItemIds, type);
+                                closeMenu();
+                            }}
+                            icon={icon}
+                        >
+                            {label}
+                        </ContextMenuItem>
+                    );
+                })}
+            >
+                Изменить форму
+            </ContextMenuItem>
+
+            <hr className="border-b-0 border-depth-6 my-1" />
+
+            <ContextMenuItem
+                onClick={() => {
+                    handlers.delete();
+                    closeMenu();
+                }}
+                disabled={selectedItemIds.length === 0}
+                shortcut="Del"
+            >
+                Удалить выбранное
+            </ContextMenuItem>
         </ContextMenu>
     );
 });
