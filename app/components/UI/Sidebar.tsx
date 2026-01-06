03e185887a1dac779d29009f78c30970ef733331
Attato <rde8519@mail.ru>
2026-01-06 23:00:39 +0300

    simplify Sidebar

--- app/components/UI/Sidebar.tsx
+++ app/components/UI/Sidebar.tsx
@@ -1,64 +1,37 @@
-import { ReactNode, memo, useEffect } from 'react';
-import { useCanvasStore } from '@/canvas/store/canvasStore';
+'use client';
+
+import { memo, useRef } from 'react';
+
+import { SidebarPanels } from '@/canvas/components/CanvasSidebar/SidebarPanels';
+import { SidebarContextMenu } from '@/canvas/components/CanvasSidebar/SidebarContextMenu';
+import { useContextMenu } from '@/canvas/hooks/useContextMenu';
 import useSidebarResize from '@/canvas/hooks/useSidebarResize';
-import { type Tab } from '@/canvas/components/CanvasSidebar/CanvasSidebar';
+import { useClickOutside } from '@/canvas/hooks/useClickOutside';
 
-type SidebarProps = {
-    minWidth?: number;
-    baseWidth?: number;
-    maxWidth?: number;
-    tabs: Tab[];
-    children: ReactNode;
-};
+export const Sidebar = memo(function Sidebar() {
+    const menu = useContextMenu();
 
-const SIDEBAR_MIN_WIDTH = 220;
-const SIDEBAR_BASE_WIDTH = 420;
-const SIDEBAR_MAX_WIDTH = typeof window !== 'undefined' ? window.innerWidth * 0.8 : 1600;
+    const sidebarRef = useRef<HTMLDivElement | null>(null);
+    const { width, startSidebarResize } = useSidebarResize();
 
-export const Sidebar = memo(function Sidebar({
-    minWidth = SIDEBAR_MIN_WIDTH,
-    baseWidth = SIDEBAR_BASE_WIDTH,
-    maxWidth = SIDEBAR_MAX_WIDTH,
-    tabs,
-    children,
-}: SidebarProps) {
-    const activeTab = useCanvasStore((state) => state.activeTab);
-
-    const { width, isResizing, startResize, open, openSidebar, closeSidebar } = useSidebarResize({
-        minWidth,
-        baseWidth,
-        maxWidth,
-        tabs: tabs.map((t) => t.id),
-    });
-
-    useEffect(() => {
-        if (activeTab !== null && !open) {
-            openSidebar();
-            return;
-        }
-
-        if (activeTab === null && open) {
-            closeSidebar();
-        }
-    }, [activeTab, open, openSidebar, closeSidebar]);
+    useClickOutside(sidebarRef, () => menu.closeMenu());
 
     return (
-        <aside
-            style={{ width: `${width}px` }}
-            className={`
-                h-screen border-l border-depth-3 bg-depth-1 select-none flex-shrink-0 z-50 relative 
-                ${!isResizing && 'transition-width ease-in-out'}
-            `}
+        <div
+            ref={sidebarRef}
+            className="absolute top-0 right-0 bottom-0 bg-depth-1 border-l border-depth-4 flex flex-col z-50 overflow-y-auto"
+            style={{ width }}
+            onContextMenu={menu.handleContextMenu}
+            onClick={() => menu.closeMenu()}
         >
-            <div className="flex flex-col h-full overflow-hidden relative">{children}</div>
-
             <div
-                onMouseDown={startResize}
-                className={`
-                    absolute top-0 left-0 h-full w-1 hover:cursor-ew-resize z-50 
-                    ${isResizing ? 'bg-depth-6' : 'hover:bg-depth-6'}
-                `}
+                onMouseDown={startSidebarResize}
+                className="absolute left-0 top-0 h-full w-1 cursor-ew-resize hover:bg-depth-3 z-50"
             />
-        </aside>
+
+            <SidebarPanels />
+
+            <SidebarContextMenu menu={menu} />
+        </div>
     );
 });
