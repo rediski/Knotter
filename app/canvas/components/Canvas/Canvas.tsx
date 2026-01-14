d4a57dd2456a6d1db7f4d47e6fef90628f1f3acc
Attato <rde8519@mail.ru>
2026-01-14 22:07:08 +0300

    delete sidebarWidth functionality and style update

--- app/canvas/components/Canvas/Canvas.tsx
+++ app/canvas/components/Canvas/Canvas.tsx
@@ -12,15 +12,11 @@ import { useCanvasInteraction } from '@/canvas/hooks/useCanvasInteraction';
 import { useCanvasRenderer } from '@/canvas/hooks/useCanvasRenderer';
 import { useContextMenu } from '@/canvas/hooks/useContextMenu';
 
-import { useCanvasStore } from '@/canvas/store/canvasStore';
-
 export default function Canvas() {
     const containerRef = useRef<HTMLDivElement | null>(null);
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
     const { selectionStart, selectionEnd, setSelectionStart, setSelectionEnd, selectItemsInArea } = useCanvasSelection();
-
-    const sidebarWidth = useCanvasStore((state) => state.sidebarWidth);
 
     useCanvasInteraction({
         containerRef,
@@ -36,16 +32,14 @@ export default function Canvas() {
     const { isOpen, position, handleContextMenu, closeMenu } = useContextMenu();
 
     return (
-        <div
-            ref={containerRef}
-            className="absolute top-0 left-0 bottom-0 overflow-hidden"
-            style={{ right: sidebarWidth }}
-            onContextMenu={handleContextMenu}
-        >
+        <div ref={containerRef} className="h-full w-full relative rounded-md bg-depth-1" onContextMenu={handleContextMenu}>
             <CanvasControls />
             <CanvasContextMenu isOpen={isOpen} position={position} closeMenu={closeMenu} />
 
-            <canvas ref={canvasRef} className="absolute top-0 left-0 w-full h-full" />
+            <canvas
+                ref={canvasRef}
+                className="absolute top-0 left-0 w-full h-full bg-depth-1 rounded-md border border-depth-3"
+            />
 
             <CanvasNodes />
             <CanvasTexts />
