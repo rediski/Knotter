54eaeb7408edab115f83e199e3ef4889ce310b8f
Attato <rde8519@mail.ru>
2026-01-25 12:18:50 +0300

    extract the logic of the element to useHierarchyitem and delete the dnd-kit

--- app/canvas/components/Hierarchy/HierarchyItem.tsx
+++ app/canvas/components/Hierarchy/HierarchyItem.tsx
@@ -1,76 +1,48 @@
 'use client';
-import { MouseEvent, KeyboardEvent, memo } from 'react';
 
-import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
-
-import { CanvasItem } from '@/canvas/canvas.types';
+import { memo } from 'react';
+import type { CanvasItem } from '@/canvas/canvas.types';
 import { EditableName } from '@/components/UI/EditableName';
+import { useHierarchyItem } from '@/canvas/components/Hierarchy/useHierarchyItem';
 import { getDynamicIcon } from '@/canvas/utils/items/getDynamicIcon';
-
-import { GripVertical } from 'lucide-react';
 
 interface HierarchyItemProps {
     canvasItem: CanvasItem;
-    isSelected: boolean;
-    onSelect: (e: MouseEvent<HTMLButtonElement>) => void;
-    onChange?: (updatedItem: CanvasItem) => void;
-    onMouseDown?: () => void;
-    onKeyDown?: (e: KeyboardEvent<HTMLButtonElement>) => void;
 }
 
-export const HierarchyItem = memo(function HierarchyItem({
-    canvasItem,
-    isSelected,
-    onSelect,
-    onChange,
-    onKeyDown,
-}: HierarchyItemProps) {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: canvasItem.id });
-
-    const style = {
-        transform: CSS.Transform.toString(transform),
-        transition,
-    };
+export const HierarchyItem = memo(function HierarchyItem({ canvasItem }: HierarchyItemProps) {
+    const { isSelected, handleSelect, handleKeyDown, handleNameChange, dragRef, dropRef, isDragOver, dragPosition } =
+        useHierarchyItem(canvasItem);
 
     const Icon = getDynamicIcon(canvasItem.kind);
 
     return (
-        <li ref={setNodeRef} style={style}>
-            <button
-                className={`w-full px-3 h-9 rounded-md outline-none tabular-nums cursor-pointer 
-                    ${
-                        isSelected
-                            ? 'bg-bg-accent/10 focus-visible:bg-bg-accent/15'
-                            : 'bg-depth-2 hover:bg-depth-3 focus-visible:bg-border'
-                    }
-                `}
-                onClick={(e) => {
-                    e.stopPropagation();
-                    onSelect(e);
-                }}
-                onKeyDown={onKeyDown}
-            >
-                <div className="flex items-center justify-between gap-1">
-                    <div className="flex items-center gap-2 w-full">
-                        <div className={`max-w-4 w-full ${isSelected ? 'text-text-accent' : 'text-foreground'} `}>
-                            <Icon size={16} />
-                        </div>
+        <li ref={dropRef} className="relative select-none" onClick={handleSelect} onKeyDown={handleKeyDown}>
+            {isDragOver && (
+                <div
+                    className={`absolute left-0 right-0 h-0.5 bg-bg-accent ${dragPosition === 'top' ? 'top-0' : 'bottom-0'}`}
+                />
+            )}
+
+            <div ref={dragRef} draggable className="cursor-grab active:cursor-grabbing">
+                <button
+                    className={`pointer-events-none w-full px-3 h-9 rounded-md outline-none tabular-nums
+                        ${
+                            isSelected
+                                ? 'bg-bg-accent/10 focus-visible:bg-bg-accent/15'
+                                : 'bg-depth-2 hover:bg-depth-3 focus-visible:bg-border'
+                        }
+                    `}
+                >
+                    <div className="flex items-center gap-2 w-full pointer-events-none">
+                        <Icon size={16} />
 
                         <div className={`border-l h-5 ${isSelected ? 'border-bg-accent/10' : 'border-depth-4'}`} />
 
-                        <EditableName
-                            name={canvasItem.name}
-                            isSelected={isSelected}
-                            onChange={(newName) => onChange?.({ ...canvasItem, name: newName })}
-                        />
+                        <EditableName name={canvasItem.name} isSelected={isSelected} onChange={handleNameChange} />
                     </div>
-
-                    <span {...attributes} {...listeners} className="cursor-grab active:cursor-grabbing">
-                        <GripVertical className="text-gray" size={16} />
-                    </span>
-                </div>
-            </button>
+                </button>
+            </div>
         </li>
     );
 });
