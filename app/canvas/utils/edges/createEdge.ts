078191fcb366590a4438ca22abef19c834f02c85
Attato <rde8519@mail.ru>
2026-02-01 20:43:36 +0300

    change from direct creation to temp edge initiation

--- app/canvas/utils/edges/createEdge.ts
+++ app/canvas/utils/edges/createEdge.ts
@@ -1,28 +1,18 @@
-import type { Edge, Node } from '@/canvas/_core/_/canvas.types';
-import { v4 as uuidv4 } from 'uuid';
+import type { Node } from '@/canvas/_core/_/canvas.types';
+import { getNodes } from '@/canvas/utils/nodes/getNodes';
+import { useCanvasStore } from '@/canvas/store/canvasStore';
 
-export function createEdge(edges: Edge[], fromNode: Node, toNode: Node): Edge {
-    const baseName = 'Связь';
-    let name = baseName;
-    let counter = 0;
+export function createEdge() {
+    const items = useCanvasStore.getState().items;
+    const selectedItemIds = useCanvasStore.getState().selectedItemIds;
+    const setTempEdge = useCanvasStore.getState().setTempEdge;
 
-    const existingNames = new Set(edges.map((edge) => edge.name));
+    if (!setTempEdge || selectedItemIds.length === 0) return;
 
-    while (existingNames.has(name)) {
-        counter++;
-        name = `${baseName} ${counter}`;
-    }
+    const nodes = getNodes(items);
+    const fromNode = nodes.find((node: Node) => node.id === selectedItemIds[0]);
 
-    return {
-        id: uuidv4(),
-        name,
-        description: '',
-        from: fromNode.id,
-        to: toNode.id,
-        position: {
-            x: (fromNode.position.x + toNode.position.x) / 2,
-            y: (fromNode.position.y + toNode.position.y) / 2,
-        },
-        kind: 'edge',
-    };
+    if (!fromNode) return;
+
+    setTempEdge({ from: fromNode.id, toPos: { ...fromNode.position } });
 }
