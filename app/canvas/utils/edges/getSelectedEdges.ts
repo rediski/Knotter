24cda67699e9745ef1f3dd57afd3f81cc6638de9
Attato <rde8519@mail.ru>
2026-02-03 21:24:57 +0300

    delete getSelectedEdges

--- app/canvas/utils/edges/getSelectedEdges.ts
+++
@@ -1,6 +0,0 @@
-import { getEdges } from '@/canvas/utils/edges/getEdges';
-
-import { CanvasItem, Edge } from '@/canvas/_core/_/canvas.types';
-
-export const getSelectedEdges = (items: CanvasItem[], selectedIds: string[]): Edge[] =>
-    getEdges(items).filter((edge) => selectedIds.includes(edge.from) && selectedIds.includes(edge.to));
