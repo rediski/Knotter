015e4b52c436bdafe15f1e9c85d1d74731389cf6
Attato <rde8519@mail.ru>
2026-02-03 21:33:22 +0300

    use getNodes in changeShapeType

--- app/canvas/utils/nodes/changeShapeType.ts
+++ app/canvas/utils/nodes/changeShapeType.ts
@@ -1,14 +1,15 @@
 import { useCanvasStore } from '@/canvas/store/canvasStore';
 import type { ShapeType } from '@/canvas/_core/_/shapeType.type';
+import { getNodes } from '@/canvas/utils/nodes/getNodes';
 
 export function changeShapeType(newShape: ShapeType) {
     const items = useCanvasStore.getState().items;
     const setItems = useCanvasStore.getState().setItems;
     const selectedItemIds = useCanvasStore.getState().selectedItemIds;
 
-    const updatedItems = items.map((item) =>
-        item.kind === 'node' && selectedItemIds.includes(item.id) ? { ...item, shapeType: newShape } : item,
-    );
+    const nodes = getNodes(items);
 
-    setItems(updatedItems);
+    const updatedNodes = nodes.map((node) => (selectedItemIds.includes(node.id) ? { ...node, shapeType: newShape } : node));
+
+    setItems(updatedNodes);
 }
