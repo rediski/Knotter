bc27133e6d1ad97f93243de24223f82bcbdf09df
Attato <rde8519@mail.ru>
2026-02-04 22:11:24 +0300

    getScreenCoords init

---
+++ app/canvas/utils/canvas/getScreenCoords.tsx
@@ -0,0 +1,19 @@
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+
+export const getScreenCoords = (x: number, y: number, containerRef: React.RefObject<HTMLDivElement | null>) => {
+    const zoomLevel = useCanvasStore.getState().zoomLevel;
+    const offset = useCanvasStore.getState().offset;
+    const invertY = useCanvasStore.getState().invertY;
+
+    const baseX = x * zoomLevel + offset.x;
+    const baseY = y * zoomLevel + offset.y;
+
+    if (invertY && containerRef.current) {
+        const containerHeight = containerRef.current.offsetHeight;
+        const screenY = -baseY + containerHeight;
+
+        return { x: baseX, y: screenY };
+    }
+
+    return { x: baseX, y: baseY };
+};
