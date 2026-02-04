35a547b17e2d35d3017c0a5b3e16ac3baa165f49
Attato <rde8519@mail.ru>
2026-02-04 20:14:26 +0300

    simplify createEdge

--- app/canvas/utils/edges/createEdge.ts
+++ app/canvas/utils/edges/createEdge.ts
@@ -1,18 +1,10 @@
-import type { Node } from '@/canvas/_core/_/canvas.types';
-import { getNodes } from '@/canvas/utils/nodes/getNodes';
 import { useCanvasStore } from '@/canvas/store/canvasStore';
 
 export function createEdge() {
-    const items = useCanvasStore.getState().items;
     const selectedItemIds = useCanvasStore.getState().selectedItemIds;
     const setTempEdge = useCanvasStore.getState().setTempEdge;
 
-    if (!setTempEdge || selectedItemIds.length === 0) return;
+    if (selectedItemIds.length === 0) return;
 
-    const nodes = getNodes(items);
-    const fromNode = nodes.find((node: Node) => node.id === selectedItemIds[0]);
-
-    if (!fromNode) return;
-
-    setTempEdge({ from: fromNode.id, toPos: { ...fromNode.position } });
+    return setTempEdge(selectedItemIds[0]);
 }
