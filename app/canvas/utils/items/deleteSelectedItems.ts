5e16519d6a611ad4478ded0eb1ad4881fb095073
Attato <rde8519@mail.ru>
2026-02-04 22:17:45 +0300

    delete getEdges

--- app/canvas/utils/items/deleteSelectedItems.ts
+++ app/canvas/utils/items/deleteSelectedItems.ts
@@ -1,7 +1,7 @@
 import { useCanvasStore } from '@/canvas/store/canvasStore';
 
 import { getNodes } from '@/canvas/utils/nodes/getNodes';
-import { getEdges } from '@/canvas/utils/edges/getEdges';
+
 import { getTexts } from '@/canvas/utils/texts/getTexts';
 
 export function deleteSelectedItems() {
@@ -10,20 +10,14 @@ export function deleteSelectedItems() {
     const selectedItemIds = useCanvasStore.getState().selectedItemIds;
 
     const nodes = getNodes(items);
-    const edges = getEdges(items);
     const texts = getTexts(items);
 
     const toDeleteNodes = new Set(nodes.filter((node) => selectedItemIds.includes(node.id)).map((node) => node.id));
-    const toDeleteEdges = new Set(edges.filter((edge) => selectedItemIds.includes(edge.id)).map((edge) => edge.id));
     const toDeleteTexts = new Set(texts.filter((text) => selectedItemIds.includes(text.id)).map((text) => text.id));
 
     const newItems = items.filter((item) => {
         if (item.kind === 'node') {
             return !toDeleteNodes.has(item.id);
-        }
-
-        if (item.kind === 'edge') {
-            return !toDeleteEdges.has(item.id) && !toDeleteNodes.has(item.from) && !toDeleteNodes.has(item.to);
         }
 
         if (item.kind === 'text') {
