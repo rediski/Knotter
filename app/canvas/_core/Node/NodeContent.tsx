d336e7d6c8395f86c67a9ce2962eaa41fcb01b0c
Attato <rde8519@mail.ru>
2026-02-06 22:28:02 +0300

    add display incoming and outgoing edges

--- app/canvas/_core/Node/NodeContent.tsx
+++ app/canvas/_core/Node/NodeContent.tsx
@@ -2,63 +2,89 @@
 
 import { NodeParameters } from '@/canvas/_core/Node/NodeParameters';
 import { useCanvasStore } from '@/canvas/store/canvasStore';
-import { getShape } from '@/canvas/utils/nodes/getShape';
+import { getNodes } from '@/canvas/utils/nodes/getNodes';
 
 export default function NodeContent() {
     const items = useCanvasStore((state) => state.items);
     const selectedTabId = useCanvasStore((state) => state.selectedTabId);
     const openedNode = items.find((item) => item.id === selectedTabId && item.kind === 'node');
 
-    if (openedNode?.kind !== 'node') return;
+    if (openedNode?.kind !== 'node') return null;
 
-    const currentShape = getShape(openedNode.shapeType);
+    const nodes = getNodes(items);
+
+    const incoming =
+        openedNode.edgeFrom?.length &&
+        openedNode.edgeFrom
+            .map((id) => nodes.find((node) => node.id === id)?.name)
+            .filter(Boolean)
+            .join(', ');
+
+    const outgoing =
+        openedNode.edgeTo?.length &&
+        openedNode.edgeTo
+            .map((id) => nodes.find((node) => node.id === id)?.name)
+            .filter(Boolean)
+            .join(', ');
 
     return (
-        <div className="flex flex-col gap-1 w-full h-full">
+        <div className="flex flex-col gap-1 w-full overflow-y-auto">
             <div className="flex gap-1">
-                <div className="flex gap-4 items-start bg-depth-1 border border-depth-3 min-w-sm w-full flex-1/3 p-3 rounded-md">
-                    <currentShape.icon className="min-w-16 h-16" />
-
+                <div className="flex-1/3 min-w-sm w-full h-fit p-3 bg-depth-1 border border-depth-3 rounded-md sticky top-0">
                     <div className="flex flex-col gap-1">
-                        <h2 className="text-lg">{openedNode.name !== '' ? openedNode.name : 'Название'}</h2>
+                        <h2 className="bg-depth-2 py-1 px-3 rounded-md">{openedNode.name || 'Название'}</h2>
 
-                        <p className="text-foreground/70 whitespace-pre-wrap break-all hyphens-auto">
-                            {openedNode.description !== '' ? openedNode.description : 'Описание'}
+                        <p className="bg-depth-2 py-1 px-3 rounded-md text-foreground/70 whitespace-pre-wrap break-all hyphens-auto">
+                            {openedNode.description || 'Описание'}
                         </p>
                     </div>
                 </div>
 
-                <div className="grid grid-cols-2 bg-depth-1 w-full border border-depth-3 rounded-md overflow-hidden text-sm">
-                    <div className="font-medium truncate p-3 border-r border-b border-depth-3">Трансформация</div>
+                <div className="flex flex-2/3 flex-col gap-1">
+                    <div className="flex flex-col gap-1 bg-depth-1 w-full border border-depth-3 rounded-md text-sm p-3">
+                        <div className="flex flex-col gap-1">
+                            <div className="pl-3 bg-depth-3 py-1 px-3 rounded-md w-fit">Трансформация</div>
 
-                    <div className="flex flex-col min-w-0 border-b border-depth-3">
-                        <div className="flex items-center justify-between border-b border-depth-3 p-3">
-                            <p className="truncate">Положение X</p>
-                            <p className="text-text-accent truncate">{openedNode.position.x}</p>
+                            <div className="flex flex-col gap-1">
+                                <div className="flex justify-between items-center bg-depth-2 py-1 px-3 rounded-md">
+                                    <span className="truncate">Положение X</span>
+                                    <span className="text-text-accent truncate">{openedNode.position.x}</span>
+                                </div>
+
+                                <div className="flex justify-between items-center bg-depth-2 py-1 px-3 rounded-md">
+                                    <span className="truncate">Положение Y</span>
+                                    <span className="text-text-accent truncate">{openedNode.position.y}</span>
+                                </div>
+                            </div>
                         </div>
 
-                        <div className="flex items-center justify-between p-3">
-                            <p className="truncate">Положение Y</p>
-                            <p className="text-text-accent truncate">{openedNode.position.y}</p>
+                        <div className="flex flex-col gap-1">
+                            <div className="pl-3 bg-depth-3 py-1 px-3 rounded-md w-fit">Связи</div>
+
+                            <div className="flex justify-between items-center bg-depth-2 py-1 px-3 rounded-md">
+                                <span className="truncate">Входящие</span>
+                                <span className="text-text-accent truncate">{incoming}</span>
+                            </div>
+
+                            <div className="flex justify-between items-center bg-depth-2 py-1 px-3 rounded-md">
+                                <span className="truncate">Исходящие</span>
+                                <span className="text-text-accent truncate">{outgoing}</span>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <div className="pl-3 bg-depth-3 py-1 px-3 rounded-md w-fit">Наследники</div>
+
+                            <div className="flex justify-between items-center bg-depth-2 py-1 px-3 rounded-md">
+                                <span className="truncate">В разработке...</span>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="font-medium truncate p-3 border-r border-b border-depth-3">Связи</div>
-
-                    <div className="p-3 border-b border-depth-3">
-                        <p className="text-right truncate">В разработке...</p>
-                    </div>
-
-                    <div className="font-medium truncate p-3 border-r border-depth-3">Наследники</div>
-
-                    <div className="p-3">
-                        <p className="text-right truncate">В разработке...</p>
+                    <div className="bg-depth-1 border border-depth-3 rounded-md p-3">
+                        <NodeParameters node={openedNode} />
                     </div>
                 </div>
-            </div>
-
-            <div className="bg-depth-1 border border-depth-3 rounded-md p-3">
-                <NodeParameters node={openedNode} />
             </div>
         </div>
     );
