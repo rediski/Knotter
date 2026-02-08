a6fa7f7d20a4e6f09baa6d5004c3e4a8f41333f0
Attato <rde8519@mail.ru>
2026-02-08 21:20:04 +0300

    fixed the movement of multiple items

--- app/canvas/utils/nodes/updateHoveredNodeId.ts
+++ app/canvas/utils/nodes/updateHoveredNodeId.ts
@@ -4,6 +4,9 @@ import { getNodeIdUnderCursor } from '@/canvas/utils/nodes/getNodeIdUnderCursor'
 export function updateHoveredNodeId(e: MouseEvent) {
     const setHoveredNodeId = useCanvasStore.getState().setHoveredNodeId;
     const hoveredNodeId = getNodeIdUnderCursor({ x: e.clientX, y: e.clientY });
+    const currentHoveredNodeId = useCanvasStore.getState().hoveredNodeId;
 
-    setHoveredNodeId(hoveredNodeId);
+    if (hoveredNodeId !== currentHoveredNodeId) {
+        setHoveredNodeId(hoveredNodeId);
+    }
 }
