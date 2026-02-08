194b012dd986e02cdbde1cf888bf25c0bdbb5c61
Attato <rde8519@mail.ru>
2026-02-08 21:32:31 +0300

    extract logic to utils and use canvas refs store

--- app/canvas/_core/Canvas/useCanvasMouseEvents.ts
+++ app/canvas/_core/Canvas/useCanvasMouseEvents.ts
@@ -1,128 +1,71 @@
 'use client';
 
-import { useCallback, RefObject, useState } from 'react';
-import type { Position } from '@/canvas/_core/_/canvas.types';
+import { RefObject, useCallback } from 'react';
 
 import { useCanvasStore } from '@/canvas/store/canvasStore';
-import { useMousePosition } from '@/canvas/_core/Canvas/useMousePosition';
+import { useCanvasRefsStore } from '@/canvas/store/canvasRefStore';
 
-import { selectItems } from '@/canvas/utils/items/selectItems';
 import { findCanvasUnderCursor } from '@/canvas/utils/canvas/findCanvasUnderCursor';
-import { getMousePosition } from '@/canvas/utils/canvas/getMousePosition';
-import { getSelectedItemsPositions } from '@/canvas/utils/items/getSelectedItemsPositions';
-import { getNodeIdUnderCursor } from '@/canvas/utils/nodes/getNodeIdUnderCursor';
-import { getTextById } from '@/canvas/utils/texts/getTextById';
-import { getTextIdUnderCursor } from '@/canvas/utils/texts/getTextIdUnderCursor';
 
 import { updateHoveredNodeId } from '@/canvas/utils/nodes/updateHoveredNodeId';
-import { createEdge } from '@/canvas/utils/edges/createEdge';
+
 import { moveItems } from '@/canvas/utils/items/moveItems';
+import { getMousePosition } from '@/canvas/utils/canvas/getMousePosition';
+import { handleClickOnItem } from '@/canvas/utils/items/handleClickOnItem';
 
-export function useCanvasMouseEvents(canvasRef: RefObject<HTMLCanvasElement | null>, isPanningRef?: RefObject<boolean>) {
+import { startDragging, stopDragging } from '@/canvas/utils/canvas/dragging';
+
+export function useCanvasMouseEvents(canvasRef: RefObject<HTMLCanvasElement | null>) {
     const items = useCanvasStore((state) => state.items);
-    const setItems = useCanvasStore((state) => state.setItems);
     const setSelectedItemIds = useCanvasStore((state) => state.setSelectedItemIds);
-    const tempEdge = useCanvasStore((state) => state.tempEdge);
-    const setMousePosition = useCanvasStore((state) => state.setMousePosition);
-
-    const { trackMousePosition } = useMousePosition();
-
-    const [pendingClickItemId, setPendingClickItemId] = useState<string | null>(null);
-    const [dragStartMouse, setDragStartMouse] = useState<Position | null>(null);
-    const [isDragging, setIsDragging] = useState(false);
-    const [initialNodePositions, setInitialNodePositions] = useState<Map<string, Position>>(new Map());
 
     const onMouseDown = useCallback(
         (e: MouseEvent) => {
-            const canvas = canvasRef.current;
-            if (!canvas) return;
+            if (!canvasRef.current) return;
+            if (findCanvasUnderCursor(e, canvasRef.current)) return;
 
-            const mousePos = getMousePosition(e, canvas);
-            trackMousePosition(mousePos, setMousePosition);
-
-            if (findCanvasUnderCursor(e, canvas)) return;
-
-            const clickedNodeId = getNodeIdUnderCursor({ x: e.clientX, y: e.clientY });
-            const clickedTextId = getTextIdUnderCursor({ x: e.clientX, y: e.clientY });
-            const clickedItemId = clickedNodeId || clickedTextId;
-
-            if (clickedNodeId) createEdge(clickedNodeId);
-
-            if (clickedTextId) {
-                const textItem = getTextById(items, clickedTextId);
-                if (textItem?.isEditing) return;
-            }
-
-            if (clickedItemId) {
-                const newSelectedIds = selectItems({ itemId: clickedItemId, event: e });
-                setSelectedItemIds(newSelectedIds);
-            }
-
-            if (!clickedItemId) {
-                if (!(e.ctrlKey || e.metaKey || e.shiftKey)) setSelectedItemIds([]);
-            }
-
-            setPendingClickItemId(clickedItemId);
-            setDragStartMouse(mousePos);
-            setInitialNodePositions(getSelectedItemsPositions());
+            handleClickOnItem(e);
+            startDragging(e, canvasRef);
         },
-        [canvasRef, items, setSelectedItemIds, trackMousePosition, setMousePosition],
+        [canvasRef, items, setSelectedItemIds],
     );
 
     const onMouseMove = useCallback(
         (e: MouseEvent) => {
-            const canvas = canvasRef.current;
-            if (!canvas) return;
+            if (!canvasRef.current) return;
+            const mousePosition = useCanvasRefsStore.getState().mousePosition;
 
-            const mousePos = getMousePosition(e, canvas);
-            trackMousePosition(mousePos, setMousePosition);
+            const isPanning = useCanvasRefsStore.getState().isPanning;
+            const isDragging = useCanvasRefsStore.getState().isDragging;
+            const dragStartMouse = useCanvasRefsStore.getState().dragStartMouse;
+            const initialNodePositions = useCanvasRefsStore.getState().initialNodePositions;
 
-            if (!isPanningRef?.current && !tempEdge && !isDragging) {
+            const tempEdge = useCanvasStore.getState().tempEdge;
+            const setItems = useCanvasStore.getState().setItems;
+
+            const mousePos = getMousePosition(e, canvasRef.current);
+
+            mousePosition.current = mousePos;
+
+            if (!isPanning?.current && !tempEdge && !isDragging.current) {
                 updateHoveredNodeId(e);
             }
 
-            if (isPanningRef?.current || !dragStartMouse || initialNodePositions.size === 0) return;
-
-            setIsDragging(true);
-
-            const dx = mousePos.x - dragStartMouse.x;
-            const dy = mousePos.y - dragStartMouse.y;
-
-            setItems(moveItems({ x: dx, y: dy }, initialNodePositions));
-        },
-        [
-            canvasRef,
-            isDragging,
-            dragStartMouse,
-            initialNodePositions,
-            setItems,
-            trackMousePosition,
-            setMousePosition,
-            tempEdge,
-            isPanningRef,
-        ],
-    );
-
-    const onMouseUp = useCallback(
-        (e: MouseEvent) => {
-            const canvas = canvasRef.current;
-            if (!canvas) return;
-
-            const mousePos = getMousePosition(e, canvas);
-            trackMousePosition(mousePos, setMousePosition);
-
-            if (!isDragging && pendingClickItemId) {
-                const newSelectedIds = selectItems({ itemId: pendingClickItemId, event: e });
-                setSelectedItemIds(newSelectedIds);
+            if (isPanning?.current || !dragStartMouse.current || initialNodePositions.current.size === 0) {
+                return;
             }
 
-            setIsDragging(false);
-            setDragStartMouse(null);
-            setPendingClickItemId(null);
-            setInitialNodePositions(new Map());
+            isDragging.current = true;
+
+            const dx = mousePos.x - dragStartMouse.current.x;
+            const dy = mousePos.y - dragStartMouse.current.y;
+
+            setItems(moveItems({ x: dx, y: dy }, initialNodePositions.current));
         },
-        [canvasRef, isDragging, pendingClickItemId, setSelectedItemIds, trackMousePosition, setMousePosition],
+        [canvasRef],
     );
+
+    const onMouseUp = useCallback(() => stopDragging(), []);
 
     return { onMouseDown, onMouseMove, onMouseUp };
 }
