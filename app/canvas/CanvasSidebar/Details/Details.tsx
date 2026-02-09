b18c16bad16ae7005452d5b93efc70b3bf6bf363
Attato <rde8519@mail.ru>
2026-02-10 00:11:05 +0300

    Details init

---
+++ app/canvas/CanvasSidebar/Details/Details.tsx
@@ -0,0 +1,39 @@
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+import { EmptyState } from '@/components/UI/EmptyState';
+import { ArrowDownToLine } from 'lucide-react';
+
+export const Details = () => {
+    const selectedItem = useCanvasStore((state) => state.selectedItem);
+
+    const handleSave = () => {
+        if (!selectedItem) return;
+
+        const blob = new Blob([JSON.stringify(selectedItem, null, 2)], { type: 'application/json' });
+
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+
+        link.href = url;
+        link.download = 'selected-item.json';
+        link.click();
+
+        URL.revokeObjectURL(url);
+    };
+
+    if (selectedItem === null) {
+        return <EmptyState message="Необходимо выбрать один из элементов" />;
+    }
+
+    return (
+        <div className="relative m-1 p-4 bg-depth-2 rounded-md space-y-3">
+            <button
+                onClick={handleSave}
+                className="absolute right-2 top-2 p-2 rounded bg-depth-3 hover:bg-depth-4 shadow text-contrast cursor-pointer"
+            >
+                <ArrowDownToLine size={16} />
+            </button>
+
+            <pre className="text-xs overflow-auto">{JSON.stringify(selectedItem, null, 2)}</pre>
+        </div>
+    );
+};
