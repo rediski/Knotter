338e8a52e3009fd56dc4a4c46b1d475a6dff1666
Attato <rde8519@mail.ru>
2026-02-16 23:41:38 +0300

    change i to item

--- app/canvas/utils/nodes/getNodes.ts
+++ app/canvas/utils/nodes/getNodes.ts
@@ -1,3 +1,3 @@
 import { CanvasItem, Node } from '@/canvas/_core/_/canvas.types';
 
-export const getNodes = (items: CanvasItem[]): Node[] => items.filter((i): i is Node => i.kind === 'node');
+export const getNodes = (items: CanvasItem[]): Node[] => items.filter((item): item is Node => item.kind === 'node');
