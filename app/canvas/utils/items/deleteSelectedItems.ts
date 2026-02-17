32057ce20122e3cee3d6a52f3f4b8c4aaab6f818
Attato <rde8519@mail.ru>
2026-02-17 23:35:19 +0300

    delete connected edges when removing nodes

--- app/canvas/utils/items/deleteSelectedItems.ts
+++ app/canvas/utils/items/deleteSelectedItems.ts
@@ -1,27 +1,28 @@
+import type { Edge } from '@/canvas/_core/_/canvas.types';
 import { useCanvasStore } from '@/canvas/store/canvasStore';
-
-import { getNodes } from '@/canvas/utils/nodes/getNodes';
-
-import { getTexts } from '@/canvas/utils/texts/getTexts';
 
 export function deleteSelectedItems() {
     const items = useCanvasStore.getState().items;
     const setItems = useCanvasStore.getState().setItems;
     const selectedItemIds = useCanvasStore.getState().selectedItemIds;
 
-    const nodes = getNodes(items);
-    const texts = getTexts(items);
+    const selectedIdsSet = new Set(selectedItemIds);
 
-    const toDeleteNodes = new Set(nodes.filter((node) => selectedItemIds.includes(node.id)).map((node) => node.id));
-    const toDeleteTexts = new Set(texts.filter((text) => selectedItemIds.includes(text.id)).map((text) => text.id));
+    const nodesToDelete = items.filter((item) => item.kind === 'node' && selectedIdsSet.has(item.id));
+
+    const deletedNodeIds = new Set(nodesToDelete.map((node) => node.id));
 
     const newItems = items.filter((item) => {
-        if (item.kind === 'node') {
-            return !toDeleteNodes.has(item.id);
+        if (selectedIdsSet.has(item.id)) {
+            return false;
         }
 
-        if (item.kind === 'text') {
-            return !toDeleteTexts.has(item.id);
+        if (item.kind === 'edge') {
+            const edge = item as Edge;
+
+            if (deletedNodeIds.has(edge.from) || deletedNodeIds.has(edge.to)) {
+                return false;
+            }
         }
 
         return true;
