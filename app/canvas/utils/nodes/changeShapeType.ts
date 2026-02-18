4ecd56aff01c90144a27ee0f62c4ef71f84fddcc
Attato <rde8519@mail.ru>
2026-02-18 21:54:48 +0300

    revent data loss by updating only selected nodes in changeShapeType

--- app/canvas/utils/nodes/changeShapeType.ts
+++ app/canvas/utils/nodes/changeShapeType.ts
@@ -1,15 +1,18 @@
 import { useCanvasStore } from '@/canvas/store/canvasStore';
 import type { ShapeType } from '@/canvas/_core/_/shapeType.type';
-import { getNodes } from '@/canvas/utils/nodes/getNodes';
 
 export function changeShapeType(newShape: ShapeType) {
     const items = useCanvasStore.getState().items;
     const setItems = useCanvasStore.getState().setItems;
     const selectedItemIds = useCanvasStore.getState().selectedItemIds;
 
-    const nodes = getNodes(items);
+    const updatedItems = items.map((item) => {
+        if (item.kind === 'node' && selectedItemIds.includes(item.id)) {
+            return { ...item, shapeType: newShape };
+        }
 
-    const updatedNodes = nodes.map((node) => (selectedItemIds.includes(node.id) ? { ...node, shapeType: newShape } : node));
+        return item;
+    });
 
-    setItems(updatedNodes);
+    setItems(updatedItems);
 }
