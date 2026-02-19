b81463db41e789906e65b823aa84080e23959a50
Attato <rde8519@mail.ru>
2026-02-20 00:26:38 +0300

    clear selected edges when selection box is empty

--- app/canvas/utils/canvas/selectItemsInSelectionBox.ts
+++ app/canvas/utils/canvas/selectItemsInSelectionBox.ts
@@ -5,7 +5,13 @@ import type { Position } from '@/canvas/_core/_/canvas.types';
 export function selectItemsInSelectionBox(start: Position, end: Position) {
     const items = useCanvasStore.getState().items;
     const setSelectedItemIds = useCanvasStore.getState().setSelectedItemIds;
+    const setSelectedEdgeIds = useCanvasStore.getState().setSelectedEdgeIds;
 
     const selected = getItemsInSelectionBox(items, start, end);
+
     setSelectedItemIds(selected);
+
+    if (selected.length === 0) {
+        setSelectedEdgeIds([]);
+    }
 }
