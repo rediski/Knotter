aa81831093264bc48fe88271eb44913115498537
Attato <rde8519@mail.ru>
2026-02-19 23:46:18 +0300

    add deleteSelectedItemsById

--- app/canvas/utils/items/deleteSelectedItems.ts
+++ app/canvas/utils/items/deleteSelectedItems.ts
@@ -1,21 +1,34 @@
-import type { Edge } from '@/canvas/_core/_/canvas.types';
+import type { Edge, Node } from '@/canvas/_core/_/canvas.types';
 import { useCanvasStore } from '@/canvas/store/canvasStore';
+import { getNodes } from '@/canvas/utils/nodes/getNodes';
 
 export function deleteSelectedItems() {
+    const selectedItemIds = useCanvasStore.getState().selectedItemIds;
+    const selectedEdgeIds = useCanvasStore.getState().selectedEdgeIds;
+
+    const idsToDelete = [...selectedItemIds, ...selectedEdgeIds];
+
+    if (idsToDelete.length === 0) return;
+
+    deleteSelectedItemsById(idsToDelete);
+}
+
+export function deleteSelectedItemsById(itemIds: string | string[]) {
     const items = useCanvasStore.getState().items;
     const setItems = useCanvasStore.getState().setItems;
     const selectedItemIds = useCanvasStore.getState().selectedItemIds;
+    const selectedEdgeIds = useCanvasStore.getState().selectedEdgeIds;
+    const setSelectedItemIds = useCanvasStore.getState().setSelectedItemIds;
+    const setSelectedEdgeIds = useCanvasStore.getState().setSelectedEdgeIds;
 
-    const selectedIdsSet = new Set(selectedItemIds);
+    const idsToDelete = new Set(Array.isArray(itemIds) ? itemIds : [itemIds]);
 
-    const nodesToDelete = items.filter((item) => item.kind === 'node' && selectedIdsSet.has(item.id));
+    const nodes = getNodes(items);
 
-    const deletedNodeIds = new Set(nodesToDelete.map((node) => node.id));
+    const deletedNodeIds = new Set(nodes.filter((node: Node) => idsToDelete.has(node.id)).map((node: Node) => node.id));
 
     const newItems = items.filter((item) => {
-        if (selectedIdsSet.has(item.id)) {
-            return false;
-        }
+        if (idsToDelete.has(item.id)) return false;
 
         if (item.kind === 'edge') {
             const edge = item as Edge;
@@ -29,4 +42,7 @@ export function deleteSelectedItems() {
     });
 
     setItems(newItems);
+
+    setSelectedItemIds(selectedItemIds.filter((id) => !idsToDelete.has(id)));
+    setSelectedEdgeIds(selectedEdgeIds.filter((id) => !idsToDelete.has(id)));
 }
