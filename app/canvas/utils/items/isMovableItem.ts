b222c3ef3cff139a258432c970fe49e3720c10cb
Attato <rde8519@mail.ru>
2026-02-21 22:26:27 +0300

    delete isMovableItem

--- app/canvas/utils/items/isMovableItem.ts
+++
@@ -1,5 +0,0 @@
-import type { CanvasItem } from '@/canvas/_core/_/canvas.types';
-
-export const isMovableItem = (item: CanvasItem): item is Exclude<CanvasItem, { kind: 'edge' }> => {
-    return item.kind !== 'edge';
-};
