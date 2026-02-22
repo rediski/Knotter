02e4044a75c3be5e02a1314cc760862b68083d11
Attato <rde8519@mail.ru>
2026-02-22 16:05:53 +0300

    add selectAllEdges in selectAllItems

--- app/canvas/utils/items/selectAllItems.ts
+++ app/canvas/utils/items/selectAllItems.ts
@@ -1,8 +1,10 @@
 import { useCanvasStore } from '@/canvas/store/canvasStore';
+import { selectAllEdges } from '@/canvas/utils/edges/selectAllEdges';
 
 export function selectAllItems() {
     const items = useCanvasStore.getState().items;
     const setSelectedItemIds = useCanvasStore.getState().setSelectedItemIds;
 
+    selectAllEdges();
     setSelectedItemIds(items.map((item) => item.id));
 }
