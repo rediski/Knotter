57d03acaa3a8d70793f6ee5277823bf4f625fe80
Attato <rde8519@mail.ru>
2026-02-28 23:54:18 +0300

    style fix

--- app/canvas/components/SidebarPanels/History/History.tsx
+++ app/canvas/components/SidebarPanels/History/History.tsx
@@ -4,14 +4,14 @@ export const History = () => {
     const clipboard = useCanvasStore((state) => state.clipboard);
 
     return (
-        <>
+        <div className="flex flex-col gap-1 m-1 text-sm">
             {clipboard.map((item) => {
                 return (
-                    <div key={item.id} className="m-1 px-3 py-1 bg-depth-2 rounded-md">
+                    <div key={item.id} className="px-3 py-1 bg-depth-2 rounded-md">
                         {item.name}
                     </div>
                 );
             })}
-        </>
+        </div>
     );
 };
