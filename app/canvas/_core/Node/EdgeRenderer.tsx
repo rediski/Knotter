14f5d9ea29802c2936d53085da10041323f234f6
Attato <rde8519@mail.ru>
2026-03-01 20:43:30 +0300

    create Edge and TempEdge components

---
+++ app/canvas/_core/Node/EdgeRenderer.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useReducer, type RefObject } from 'react';
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+import { getEdges } from '@/canvas/utils/edges/getEdges';
+
+import { Edge } from '@/canvas/_core/Node/Edge';
+import { TempEdge } from '@/canvas/_core/Node/TempEdge';
+
+export const EdgeRenderer = ({ containerRef }: { containerRef: RefObject<HTMLDivElement | null> }) => {
+    const items = useCanvasStore((state) => state.items);
+    const tempEdge = useCanvasStore((state) => state.tempEdge);
+
+    const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+    useEffect(() => {
+        if (!tempEdge) return;
+
+        let frame: number | null = null;
+
+        const handleMove = () => {
+            if (frame !== null) return;
+
+            frame = requestAnimationFrame(() => {
+                forceUpdate();
+                frame = null;
+            });
+        };
+
+        window.addEventListener('mousemove', handleMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMove);
+            if (frame !== null) cancelAnimationFrame(frame);
+        };
+    }, [tempEdge]);
+
+    const edges = getEdges(items);
+
+    return (
+        <svg className="absolute inset-0 w-full h-full pointer-events-none">
+            {edges.map((edge) => (
+                <Edge key={edge.id} edge={edge} containerRef={containerRef} />
+            ))}
+
+            {tempEdge && <TempEdge containerRef={containerRef} />}
+        </svg>
+    );
+};
