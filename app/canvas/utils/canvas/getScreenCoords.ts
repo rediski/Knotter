ec6838a7de09841f9d0f966a88ced65ef94023fd
Attato <rde8519@mail.ru>
2026-03-03 21:28:07 +0300

    use single getState call

--- app/canvas/utils/canvas/getScreenCoords.ts
+++ app/canvas/utils/canvas/getScreenCoords.ts
@@ -1,9 +1,11 @@
 import { useCanvasStore } from '@/canvas/store/useCanvasStore';
 
 export const getScreenCoords = (x: number, y: number, containerRef: React.RefObject<HTMLDivElement | null>) => {
-    const zoomLevel = useCanvasStore.getState().zoomLevel;
-    const offset = useCanvasStore.getState().offset;
-    const invertY = useCanvasStore.getState().invertY;
+    const canvasState = useCanvasStore.getState();
+
+    const zoomLevel = canvasState.zoomLevel;
+    const offset = canvasState.offset;
+    const invertY = canvasState.invertY;
 
     const baseX = x * zoomLevel + offset.x;
     const baseY = y * zoomLevel + offset.y;
