d84b74b4f9260a3cfa27087cebac8693ec913987
Attato <rde8519@mail.ru>
2026-03-07 20:32:33 +0300

    add getSelectedItem and convert getSelectedItems to pure function

--- app/canvas/utils/items/getSelectedItem.ts
+++
@@ -1,8 +0,0 @@
-import type { CanvasItem } from '@/canvas/_core/_/canvas.types';
-import { getSelectedItems } from '@/canvas/utils/items/getSelectedItems';
-
-export const getSelectedItem = (): CanvasItem | null => {
-    const selectedItems = getSelectedItems();
-
-    return selectedItems[0] || null;
-};
--- app/canvas/utils/items/getSelectedItems.ts
+++ app/canvas/utils/items/getSelectedItems.ts
@@ -1,14 +1,15 @@
-import { useItemsStore } from '@/canvas/store/useItemsStore';
 import type { CanvasItem } from '@/canvas/_core/_/canvas.types';
+import type { getSelectedItemsParams } from '@/canvas/_core/_/items.types';
 
-export const getSelectedItems = (): CanvasItem[] => {
-    const itemsState = useItemsStore.getState();
-
-    const items = itemsState.items;
-    const selectedItemIds = itemsState.selectedItemIds;
-
+export const getSelectedItems = ({ items, selectedItemIds }: getSelectedItemsParams): CanvasItem[] => {
     const selectedIdsSet = new Set(selectedItemIds);
     const selectedItems = items.filter((item) => selectedIdsSet.has(item.id));
 
     return selectedItems;
+};
+
+export const getSelectedItem = ({ items, selectedItemIds }: getSelectedItemsParams): CanvasItem | null => {
+    const selectedItems = getSelectedItems({ items, selectedItemIds });
+
+    return selectedItems[0] || null;
 };
