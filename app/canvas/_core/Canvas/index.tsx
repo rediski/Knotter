d9586714b32a64360423c5261a315da0cb624c02
Attato <rde8519@mail.ru>
2026-03-07 14:19:18 +0300

    add loading state and hydration check

--- app/canvas/_core/Canvas/index.tsx
+++ app/canvas/_core/Canvas/index.tsx
@@ -11,22 +11,31 @@ import { CanvasControls } from '@/canvas/components/canvas/CanvasControls';
 import { useCanvasInteraction } from '@/canvas/_core/Canvas/useCanvasInteraction';
 import { useCanvasRenderer } from '@/canvas/_core/Canvas/useCanvasRenderer';
 import { useCanvasHotkeys } from '@/canvas/_core/Canvas/useCanvasHotkeys';
+import { useStoreHydration } from '@/hooks/useStoreHydration';
 
 import { useContextMenu } from '@/hooks/useContextMenu';
+import { useCanvasStore } from '@/canvas/store/useCanvasStore';
+import { useItemsStore } from '@/canvas/store/useItemsStore';
+
+import { getNodes } from '@/canvas/utils/nodes/getNodes';
 
 export default function Canvas() {
     const containerRef = useRef<HTMLDivElement | null>(null);
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    useCanvasInteraction({
-        containerRef,
-        canvasRef,
-    });
+    const isCanvasHydrated = useStoreHydration(useCanvasStore);
+    const isItemsHydrated = useStoreHydration(useItemsStore);
 
+    useCanvasInteraction({ containerRef, canvasRef });
     useCanvasHotkeys(canvasRef);
     useCanvasRenderer(canvasRef);
 
     const { isOpen, position, handleContextMenu, closeMenu } = useContextMenu();
+
+    const items = useItemsStore((state) => state.items);
+    const hasNodes = getNodes(items).length !== 0;
+
+    const isReady = isCanvasHydrated && isItemsHydrated;
 
     return (
         <div ref={containerRef} className="h-full w-full relative rounded-md bg-depth-1" onContextMenu={handleContextMenu}>
@@ -40,9 +49,17 @@ export default function Canvas() {
 
             <SelectionBox containerRef={containerRef} />
 
-            <div className="absolute inset-0 overflow-hidden pointer-events-none">
-                <Node containerRef={containerRef} />
-            </div>
+            {isReady && hasNodes && (
+                <div className="absolute inset-0 overflow-hidden pointer-events-none">
+                    <Node containerRef={containerRef} />
+                </div>
+            )}
+
+            {!isReady && (
+                <div className="absolute inset-0 flex items-center justify-center bg-depth-1">
+                    <div className="text-foreground">Загрузка сцены...</div>
+                </div>
+            )}
         </div>
     );
 }
