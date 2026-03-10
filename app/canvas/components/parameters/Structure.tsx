7fb4a6286b2cb58b7cdcfe10ccca98140fd407c4
Attato <rde8519@mail.ru>
2026-03-10 23:54:22 +0300

    add EditableName

--- app/canvas/components/parameters/Structure.tsx
+++ app/canvas/components/parameters/Structure.tsx
@@ -5,8 +5,10 @@ import { Parameter } from '@/canvas/_core/_/parameter';
 import { isStructure } from '@/canvas/_core/_/parameter.type-guards';
 
 import { CreateParameterForm } from '@/canvas/components/sidebar/CreateParameterForm';
+import { EditableName } from '@/components/UI/EditableName';
 import { EmptyState } from '@/components/UI/EmptyState';
 
+import { updateParameterName } from '@/canvas/utils/parameters/updateParameterName';
 import { removeParameter } from '@/canvas/utils/parameters/removeParameter';
 
 import { Folder, X } from 'lucide-react';
@@ -15,9 +17,15 @@ export const Structure = memo(function Structure({ parameter }: { parameter: Par
     if (!isStructure(parameter)) return null;
 
     return (
-        <div className="flex flex-col gap-1 px-3 py-2 bg-depth-2 text-sm rounded-md">
-            <div className="flex items-center gap-1 h-8">
+        <div className="flex flex-col gap-1 px-3 py-1 bg-depth-2 text-sm rounded-md">
+            <div className="flex items-center gap-2 h-8">
                 <Folder size={16} className="min-w-4" />
+
+                <EditableName
+                    name={parameter.name}
+                    onChange={(newName) => updateParameterName(parameter.id, newName)}
+                    className="w-full"
+                />
 
                 <button onClick={() => removeParameter(parameter.id)} className="ml-auto text-gray cursor-pointer">
                     <X size={16} />
