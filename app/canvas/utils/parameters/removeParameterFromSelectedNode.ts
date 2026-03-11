3098bbb7264cfe7cdb4072b14ac167059fa33808
Attato <rde8519@mail.ru>
2026-03-11 21:48:05 +0300

    removeParameterFromSelectedNode init

---
+++ app/canvas/utils/parameters/removeParameterFromSelectedNode.ts
@@ -0,0 +1,29 @@
+import { useItemsStore } from '@/canvas/store/useItemsStore';
+import { getSelectedNode } from '@/canvas/utils/nodes/getSelectedNodes';
+
+export const removeParameterFromSelectedNode = (parameterId: string) => {
+    const itemsState = useItemsStore.getState();
+
+    const items = itemsState.items;
+    const setItems = itemsState.setItems;
+    const selectedItemIds = itemsState.selectedItemIds;
+
+    const node = getSelectedNode({ items, selectedItemIds });
+
+    if (!node) return;
+
+    const parameterExists = node.parameters.some((parameter) => parameter.id === parameterId);
+    if (!parameterExists) return;
+
+    const updatedItems = items.map((item) => {
+        if (item.kind === 'node' && item.id === node.id) {
+            return {
+                ...item,
+                parameters: item.parameters.filter((parameter) => parameter.id !== parameterId),
+            };
+        }
+        return item;
+    });
+
+    setItems(updatedItems);
+};
