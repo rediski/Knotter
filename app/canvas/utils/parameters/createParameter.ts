1cfa2d3f84f2c4b1edfba3b0320cb56eb44a4bdc
Attato <rde8519@mail.ru>
2026-03-12 22:22:54 +0300

    createStructureParameter init

--- app/canvas/utils/parameters/createParameter.ts
+++ app/canvas/utils/parameters/createParameter.ts
@@ -2,6 +2,7 @@ import type { ParameterType, Parameter } from '@/canvas/_core/_/parameter';
 import { parameterInitialValue } from '@/canvas/utils/parameters/parameterInitialValue';
 
 import { useItemsStore } from '@/canvas/store/useItemsStore';
+import { isStructure } from '@/canvas/_core/_/parameter.type-guards';
 
 import { v4 as uuid } from 'uuid';
 
@@ -19,6 +20,36 @@ export const createParameter = (name: string, type: ParameterType): Parameter =>
     } as Parameter;
 
     setParameters([...parameters, newParameter]);
+
+    return newParameter;
+};
+
+export const createStructureParameter = (name: string, type: ParameterType, parentStructureId: string): Parameter => {
+    const itemsState = useItemsStore.getState();
+    const parameters = itemsState.parameters;
+    const setParameters = itemsState.setParameters;
+
+    const parentStructure = parameters.find((p) => p.id === parentStructureId);
+
+    if (!parentStructure || !isStructure(parentStructure)) {
+        throw new Error('Родительская структура не найдена или имеет неверный тип');
+    }
+
+    const newParameter: Parameter = {
+        id: uuid(),
+        name,
+        type,
+        data: parameterInitialValue(type),
+    } as Parameter;
+
+    const updatedParentStructure: Parameter<'structure'> = {
+        ...parentStructure,
+        data: [...parentStructure.data, newParameter],
+    };
+
+    const updatedParameters = parameters.map((p) => (p.id === parentStructureId ? updatedParentStructure : p));
+
+    setParameters(updatedParameters);
 
     return newParameter;
 };
