ff1cad82fafab156056774e8bd9aa755eb4e8438
Attato <rde8519@mail.ru>
2026-03-16 21:46:13 +0300

    simplify boolean parameter

--- app/canvas/components/parameters/Boolean.tsx
+++ app/canvas/components/parameters/Boolean.tsx
@@ -6,21 +6,13 @@ import type { Parameter } from '@/canvas/_core/_/parameter';
 import { isBoolean } from '@/canvas/_core/_/parameter.type-guards';
 
 import { EditableName } from '@/components/UI/EditableName';
-import { Checkbox } from '@/components/UI/Checkbox';
 
-import { updateParameter } from '@/canvas/utils/parameters/updateParameter';
 import { updateParameterName } from '@/canvas/utils/parameters/updateParameterName';
 import { removeParameter } from '@/canvas/utils/parameters/removeParameter';
 
 import { FlagTriangleRight, X } from 'lucide-react';
 
 export const Boolean = memo(function Boolean({ parameter }: { parameter: Parameter }) {
-    const handleCheckboxChange = (checked: boolean) => {
-        updateParameter(parameter.id, {
-            data: checked,
-        });
-    };
-
     if (!parameter) return;
     if (!isBoolean(parameter)) return;
 
@@ -38,22 +30,6 @@ export const Boolean = memo(function Boolean({ parameter }: { parameter: Paramet
                 <button onClick={() => removeParameter(parameter.id)} className="ml-auto text-gray cursor-pointer">
                     <X size={16} />
                 </button>
-            </div>
-
-            <div className="flex items-center gap-1">
-                <div className="flex flex-col gap-1 w-full">
-                    <div className="flex items-center gap-2">
-                        <p className="truncate w-full text-right">Базовое значение</p>
-
-                        <div className="w-full">
-                            <Checkbox
-                                checked={parameter.data}
-                                onChange={(checked) => handleCheckboxChange(checked)}
-                                className="bg-depth-3 border border-depth-4"
-                            />
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     );
