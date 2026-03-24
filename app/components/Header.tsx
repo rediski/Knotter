3bcc8b71d82973c1d0ffcde5bfecf70bcd534016
Attato <rde8519@mail.ru>
2026-03-24 22:46:58 +0300

    redesign header and add link to canvas

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,17 +6,24 @@ import { ThemeToggle } from '@/components/ThemeToggle';
 
 export default function Header() {
     return (
-        <header className="sticky top-0 w-full border-b border-depth-3 px-4 bg-background z-50">
-            <div className="container flex justify-between items-center m-auto h-16">
-                <div className="flex gap-6 items-center text-[15px]">
-                    <Link href="/" className="tracking-widest font-extrabold text-base select-none">
-                        KNTTR
+        <header className="sticky top-4 w-full border-depth-3 bg-background z-50">
+            <div className="container flex justify-between items-center bg-depth-1 border border-depth-3 px-6 py-1 rounded-md mx-auto">
+                <div className="flex items-center">
+                    <Link href="/" className="tracking-wide font-extrabold text-base select-none uppercase">
+                        Knotter
                     </Link>
                 </div>
 
-                <div className="flex items-center gap-3">
+                <div className="flex items-center gap-1">
                     <ThemeToggle />
                     <GithubBager />
+
+                    <Link
+                        href="/canvas"
+                        className="flex items-center w-fit text-sm px-3 h-8 bg-bg-accent/10 hover:bg-bg-accent/15 border border-bg-accent/15 hover:border-bg-accent/20 transition-colors text-text-accent rounded-lg select-none"
+                    >
+                        Open Workspace
+                    </Link>
                 </div>
             </div>
         </header>
