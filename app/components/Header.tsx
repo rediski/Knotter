fe5e2131fd816144f157847c1732be2dd829f82b
Attato <rde8519@mail.ru>
2026-03-31 19:53:11 +0300

    add backdrop gradient

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,26 +7,35 @@ import { GithubBager } from '@/components/GithubBager';
 
 export default function Header() {
     return (
-        <header className="sticky top-4 w-full border-depth-3 z-50">
-            <div className="container flex justify-between items-center bg-depth-1 border border-depth-3 px-6 py-1 rounded-md mx-auto">
-                <div className="flex items-center">
-                    <Link href="/" className="tracking-wide font-extrabold text-base select-none uppercase">
-                        Knotter
-                    </Link>
-                </div>
+        <>
+            <div className="fixed -top-1 left-0 w-full h-15 bg-linear-to-b from-depth-1 to-transparent backdrop-blur-xs z-40" />
 
-                <div className="flex items-center gap-1">
-                    <ThemeToggle />
-                    <GithubBager />
+            <header className="sticky top-4 w-full z-50">
+                <div className="container flex justify-between items-center bg-depth-1 border border-depth-3 px-6 py-1 rounded-md mx-auto">
+                    <div className="flex items-center">
+                        <Link href="/" className="tracking-wide font-extrabold text-base select-none uppercase">
+                            Knotter
+                        </Link>
+                    </div>
 
-                    <Link
-                        href="/canvas"
-                        className="flex items-center w-fit text-sm px-3 h-8 bg-bg-accent/10 hover:bg-bg-accent/15 border border-bg-accent/15 hover:border-bg-accent/20 transition-colors text-text-accent rounded-lg select-none"
-                    >
-                        Open Workspace
-                    </Link>
+                    <div className="flex items-center gap-1">
+                        <div className="flex gap-3">
+                            <GithubBager />
+
+                            <div className="my-1 border-l border-depth-3" />
+
+                            <ThemeToggle />
+                        </div>
+
+                        <Link
+                            href="/canvas"
+                            className="flex items-center w-fit text-sm px-3 h-8 bg-bg-accent/10 hover:bg-bg-accent/15 border border-bg-accent/15 hover:border-bg-accent/20 transition-colors text-text-accent rounded-lg select-none"
+                        >
+                            Open Workspace
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
     );
 }
