3462a4e66a01b6101cb42aee49640949b1a08c11
Attato <rde8519@mail.ru>
2026-04-09 20:51:19 +0300

    change base value for number parameter

--- app/canvas/utils/parameters/parameterInitialValue.ts
+++ app/canvas/utils/parameters/parameterInitialValue.ts
@@ -1,7 +1,7 @@
 import type { ParameterType, Parameter, ParameterTypeMap } from '@/canvas/_core/_/parameter';
 
 const INITIAL_PARAMETER_VALUES = {
-    number: { value: 0, min: undefined, max: undefined } satisfies ParameterTypeMap['number'],
+    number: 0 satisfies ParameterTypeMap['number'],
     string: '' satisfies ParameterTypeMap['string'],
     boolean: false satisfies ParameterTypeMap['boolean'],
     enum: { value: null, options: [] } satisfies ParameterTypeMap['enum'],
