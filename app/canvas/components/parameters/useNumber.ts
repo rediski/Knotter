046323417de323f3dec9ddd3ff92ebb5461b1203
Attato <rde8519@mail.ru>
2026-04-09 20:53:01 +0300

    remove min/max fields

--- app/canvas/components/parameters/useNumber.ts
+++ app/canvas/components/parameters/useNumber.ts
@@ -1,4 +1,4 @@
-import { Parameter, ParameterTypeMap } from '@/canvas/_core/_/parameter';
+import { Parameter } from '@/canvas/_core/_/parameter';
 import { isNumber } from '@/canvas/_core/_/parameter.type-guards';
 
 interface useNumberParameterProps {
@@ -9,8 +9,7 @@ interface useNumberParameterProps {
 export const useNumberParameter = ({ parameter, updateParameter }: useNumberParameterProps) => {
     if (!parameter || !isNumber(parameter)) {
         return {
-            handleUpdateMinValue: () => {},
-            handleUpdateMaxValue: () => {},
+            handleUpdateValue: () => {},
         };
     }
 
@@ -20,42 +19,16 @@ export const useNumberParameter = ({ parameter, updateParameter }: useNumberPara
         return isNaN(num) ? undefined : num;
     };
 
-    const data = parameter.data as ParameterTypeMap['number'];
-
-    const handleUpdateMinValue = (value: string) => {
-        const numValue = parseNumber(value);
-
-        if (numValue !== undefined && data.max !== undefined && numValue > data.max) {
-            return;
-        }
+    const handleUpdateValue = (value: string) => {
+        const numValue = parseNumber(value) ?? 0;
 
         updateParameter(parameter.id, {
             ...parameter,
-            data: {
-                ...data,
-                min: numValue,
-            },
-        });
-    };
-
-    const handleUpdateMaxValue = (value: string) => {
-        const numValue = parseNumber(value);
-
-        if (numValue !== undefined && data.min !== undefined && numValue < data.min) {
-            return;
-        }
-
-        updateParameter(parameter.id, {
-            ...parameter,
-            data: {
-                ...data,
-                max: numValue,
-            },
+            data: numValue,
         });
     };
 
     return {
-        handleUpdateMinValue,
-        handleUpdateMaxValue,
+        handleUpdateValue,
     };
 };
