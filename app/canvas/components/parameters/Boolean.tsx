d12e3c1dd29915025426e725f0c118b4279fb792
Attato <rde8519@mail.ru>
2026-04-10 22:04:22 +0300

    add checkbox for value editing

--- app/canvas/components/parameters/Boolean.tsx
+++ app/canvas/components/parameters/Boolean.tsx
@@ -6,26 +6,38 @@ import type { Parameter } from '@/canvas/_core/_/parameter';
 import { isBoolean } from '@/canvas/_core/_/parameter.type-guards';
 
 import { EditableName } from '@/components/UI/EditableName';
+import { Checkbox } from '@/components/UI/Checkbox';
 
 import { updateParameterName } from '@/canvas/utils/parameters/updateParameterName';
 import { removeParameter } from '@/canvas/utils/parameters/removeParameter';
+import { updateParameter } from '@/canvas/utils/parameters/updateParameter';
 
-import { FlagTriangleRight, X } from 'lucide-react';
+import { X } from 'lucide-react';
 
 export const Boolean = memo(function Boolean({ parameter }: { parameter: Parameter }) {
-    if (!parameter) return;
-    if (!isBoolean(parameter)) return;
+    if (!parameter || !isBoolean(parameter)) {
+        return null;
+    }
+
+    const handleUpdateValue = (checked: boolean) => {
+        updateParameter(parameter.id, {
+            ...parameter,
+            data: checked,
+        });
+    };
 
     return (
         <div className="flex flex-col justify-center gap-2 px-3 py-1 text-sm bg-depth-2 border border-depth-3 rounded-md">
             <div className="flex items-center gap-2 h-8">
-                <FlagTriangleRight size={16} className="min-w-4" />
+                <div className="w-2 h-2 bg-json-boolean rounded-full" />
 
                 <EditableName
                     name={parameter.name}
                     onChange={(newName) => updateParameterName(parameter.id, newName)}
-                    className="w-full"
+                    className="w-full text-json-boolean"
                 />
+
+                <Checkbox checked={parameter.data} onChange={handleUpdateValue} />
 
                 <button onClick={() => removeParameter(parameter.id)} className="ml-auto text-gray cursor-pointer">
                     <X size={16} />
