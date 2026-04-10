e99a35260475c8b32552e15887e0dac689a0eac8
Attato <rde8519@mail.ru>
2026-04-10 22:06:14 +0300

    add value input and unify visual style

--- app/canvas/components/parameters/String.tsx
+++ app/canvas/components/parameters/String.tsx
@@ -6,25 +6,42 @@ import type { Parameter } from '@/canvas/_core/_/parameter';
 import { isString } from '@/canvas/_core/_/parameter.type-guards';
 
 import { EditableName } from '@/components/UI/EditableName';
+import { Input } from '@/components/UI/Input';
 
+import { useStringParameter } from '@/canvas/components/parameters/useStringParameter';
+
+import { updateParameter } from '@/canvas/utils/parameters/updateParameter';
 import { updateParameterName } from '@/canvas/utils/parameters/updateParameterName';
 import { removeParameter } from '@/canvas/utils/parameters/removeParameter';
 
-import { Type, X } from 'lucide-react';
+import { X } from 'lucide-react';
 
 export const String = memo(function String({ parameter }: { parameter: Parameter }) {
+    const { handleUpdateValue } = useStringParameter({
+        parameter,
+        updateParameter,
+    });
+
     if (!parameter) return;
     if (!isString(parameter)) return;
 
     return (
         <div className="flex flex-col justify-center gap-2 px-3 py-1 text-sm bg-depth-2 border border-depth-3 rounded-md">
             <div className="flex items-center gap-2 h-8">
-                <Type size={16} className="min-w-4" />
+                <div className="min-w-2 h-2 bg-json-string rounded-full" />
 
                 <EditableName
                     name={parameter.name}
                     onChange={(newName) => updateParameterName(parameter.id, newName)}
-                    className="w-full"
+                    className="w-full text-json-string"
+                />
+
+                <Input
+                    value={parameter.data}
+                    onChange={handleUpdateValue}
+                    className="bg-depth-3 border border-depth-4"
+                    type="number"
+                    placeholder="Введите значение"
                 />
 
                 <button onClick={() => removeParameter(parameter.id)} className="ml-auto text-gray cursor-pointer">
