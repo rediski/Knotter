9022e91c3378f43ab934bf07a691630553437824
Attato <rde8519@mail.ru>
2026-04-11 22:36:39 +0300

    simplify number parameter

--- app/canvas/components/parameters/Number.tsx
+++ app/canvas/components/parameters/Number.tsx
@@ -12,9 +12,6 @@ import { useNumberParameter } from '@/canvas/components/parameters/useNumber';
 
 import { updateParameter } from '@/canvas/utils/parameters/updateParameter';
 import { updateParameterName } from '@/canvas/utils/parameters/updateParameterName';
-import { removeParameter } from '@/canvas/utils/parameters/removeParameter';
-
-import { X } from 'lucide-react';
 
 export const Number = memo(function Number({ parameter }: { parameter: Parameter }) {
     const { handleUpdateValue } = useNumberParameter({
@@ -25,28 +22,22 @@ export const Number = memo(function Number({ parameter }: { parameter: Parameter
     if (!isNumber(parameter)) return null;
 
     return (
-        <div className="flex flex-col justify-center gap-2 px-3 py-1 text-sm bg-depth-2 border border-depth-3 rounded-md">
-            <div className="flex items-center gap-2 h-8">
-                <div className="min-w-2 h-2 bg-json-number rounded-full" />
+        <div className="flex items-center gap-2 h-8 w-full">
+            <div className="min-w-2 h-2 bg-json-number rounded-full" />
 
-                <EditableName
-                    name={parameter.name}
-                    onChange={(newName) => updateParameterName(parameter.id, newName)}
-                    className="w-full text-json-number"
-                />
+            <EditableName
+                name={parameter.name}
+                onChange={(newName) => updateParameterName(parameter.id, newName)}
+                className="w-full text-json-number"
+            />
 
-                <Input
-                    value={String(parameter.data ?? '')}
-                    onChange={handleUpdateValue}
-                    className="bg-depth-3 border border-depth-4"
-                    type="number"
-                    placeholder="Введите значение"
-                />
-
-                <button onClick={() => removeParameter(parameter.id)} className="ml-auto text-gray cursor-pointer">
-                    <X size={16} />
-                </button>
-            </div>
+            <Input
+                value={String(parameter.data ?? '')}
+                onChange={handleUpdateValue}
+                className="bg-depth-3 border border-depth-4"
+                type="number"
+                placeholder="Введите значение"
+            />
         </div>
     );
 });
