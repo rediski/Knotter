8d45fa6e92005cb47c8ed55e9814d351581a3dc1
Attato <rde8519@mail.ru>
2026-04-18 22:39:37 +0300

    replace nested parameter data with flat store structure

--- app/canvas/utils/parameters/addParameterToStructure.ts
+++ app/canvas/utils/parameters/addParameterToStructure.ts
@@ -2,6 +2,7 @@ import { Parameter } from '@/canvas/_core/_/parameter';
 import { isStructure } from '@/canvas/_core/_/parameter.type-guards';
 import { updateParameter } from '@/canvas/utils/parameters/updateParameter';
 import { parameterInitialValue } from '@/canvas/utils/parameters/parameterInitialValue';
+import { useItemsStore } from '@/canvas/store/useItemsStore';
 import { v4 as uuid } from 'uuid';
 
 interface AddParameterToStructureProps {
@@ -14,15 +15,20 @@ export const addParameterToStructure = ({ parameter, name, type }: AddParameterT
     if (!parameter) return;
     if (!isStructure(parameter)) return;
 
-    const newParam: Parameter = {
+    const itemsState = useItemsStore.getState();
+
+    const newParameter: Parameter = {
         id: uuid(),
         name,
         type,
         data: parameterInitialValue(type),
+        parentId: parameter.id,
     } as Parameter;
+
+    itemsState.setParameters([...itemsState.parameters, newParameter]);
 
     updateParameter(parameter.id, {
         ...parameter,
-        data: [...parameter.data, newParam],
+        data: [...parameter.data, newParameter.id],
     });
 };
