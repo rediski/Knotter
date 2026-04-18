7a1e2649b617504bab24b229d859c017ebf9f2cc
Attato <rde8519@mail.ru>
2026-04-18 22:40:06 +0300

    remove recursive update for flattened parameter structure

--- app/canvas/utils/parameters/updateParameter.ts
+++ app/canvas/utils/parameters/updateParameter.ts
@@ -1,29 +1,19 @@
 import type { Parameter } from '@/canvas/_core/_/parameter';
-import { isStructure } from '@/canvas/_core/_/parameter.type-guards';
+
 import { useItemsStore } from '@/canvas/store/useItemsStore';
 
 export const updateParameter = (parameterId: string, updates: Partial<Parameter>) => {
     const itemsState = useItemsStore.getState();
-
     const parameters = itemsState.parameters;
     const setParameters = itemsState.setParameters;
 
-    const updateRecursive = (params: Parameter[]): Parameter[] => {
-        return params.map((parameter) => {
-            if (parameter.id === parameterId) {
-                return { ...parameter, ...updates } as Parameter;
-            }
+    const updatedParameters = parameters.map((parameter) => {
+        if (parameter.id === parameterId) {
+            return { ...parameter, ...updates } as Parameter;
+        }
 
-            if (isStructure(parameter)) {
-                return {
-                    ...parameter,
-                    data: updateRecursive(parameter.data),
-                };
-            }
+        return parameter;
+    });
 
-            return parameter;
-        });
-    };
-
-    setParameters(updateRecursive(parameters));
+    setParameters(updatedParameters);
 };
