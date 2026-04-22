671cbdc5878825fc2964cb1575b1d3f5bca366dd
Attato <rde8519@mail.ru>
2026-04-22 22:25:40 +0300

    operate on parameter.value instead of defaultValue

--- app/canvas/components/parameters/useEnum.ts
+++ app/canvas/components/parameters/useEnum.ts
@@ -2,32 +2,27 @@ import { Parameter } from '@/canvas/_core/_/parameter';
 import { updateParameter } from '@/canvas/utils/parameters/updateParameter';
 import { isEnum } from '@/canvas/_core/_/parameter.type-guards';
 
-export const useEnum = ({ parameter }: { parameter: Parameter | undefined }) => {
+export const useEnum = ({ parameter }: { parameter: Parameter }) => {
     const addEnumOption = () => {
         if (!parameter) return;
         if (!isEnum(parameter)) return;
 
-        let parameterDefaultValue = parameter.defaultValue;
+        const currentValue = parameter.value as { selected: string | null; options: string[] };
+        const ordinalNumber = currentValue.options.length + 1;
 
-        if (!parameterDefaultValue || !parameterDefaultValue.options) {
-            parameterDefaultValue = { value: null, options: [] };
-        }
-
-        const ordinalNumber = parameterDefaultValue.options.length + 1;
-
-        let newValue = `Опция ${ordinalNumber}`;
+        let newOption = `Опция ${ordinalNumber}`;
         let counter = 1;
 
-        while (parameterDefaultValue.options.includes(newValue)) {
-            newValue = `Опция ${ordinalNumber} (${counter})`;
+        while (currentValue.options.includes(newOption)) {
+            newOption = `Опция ${ordinalNumber} (${counter})`;
             counter++;
         }
 
         updateParameter(parameter.id, {
             ...parameter,
-            defaultValue: {
-                ...parameterDefaultValue,
-                options: [...parameterDefaultValue.options, newValue],
+            value: {
+                ...currentValue,
+                options: [...currentValue.options, newOption],
             },
         });
     };
@@ -36,19 +31,18 @@ export const useEnum = ({ parameter }: { parameter: Parameter | undefined }) => 
         if (!parameter) return;
         if (!isEnum(parameter)) return;
 
-        let parameterDefaultValue = parameter.defaultValue;
+        const currentValue = parameter.value as { selected: string | null; options: string[] };
+        const updatedOptions = currentValue.options.filter((_, i) => i !== index);
 
-        if (!parameterDefaultValue || !parameterDefaultValue.options) {
-            parameterDefaultValue = { value: null, options: [] };
-        }
-
-        const updatedOptions = parameterDefaultValue.options.filter((_, i) => i !== index);
+        const newSelected =
+            currentValue.selected && updatedOptions.includes(currentValue.selected) ? currentValue.selected : null;
 
         updateParameter(parameter.id, {
             ...parameter,
-            defaultValue: {
-                ...parameterDefaultValue,
+            value: {
+                ...currentValue,
                 options: updatedOptions,
+                selected: newSelected,
             },
         });
     };
@@ -57,23 +51,22 @@ export const useEnum = ({ parameter }: { parameter: Parameter | undefined }) => 
         if (!parameter) return;
         if (!isEnum(parameter)) return;
 
-        let parameterDefaultValue = parameter.defaultValue;
+        const currentValue = parameter.value as { selected: string | null; options: string[] };
 
-        if (!parameterDefaultValue || !parameterDefaultValue.options) {
-            parameterDefaultValue = { value: null, options: [] };
-        }
-
-        if (parameterDefaultValue.options.some((option, i) => i !== index && option === newValue)) {
+        if (currentValue.options.some((option, i) => i !== index && option === newValue)) {
             return;
         }
 
-        const updatedOptions = parameterDefaultValue.options.map((option, i) => (i === index ? newValue : option));
+        const updatedOptions = currentValue.options.map((option, i) => (i === index ? newValue : option));
+
+        const newSelected = currentValue.selected === currentValue.options[index] ? newValue : currentValue.selected;
 
         updateParameter(parameter.id, {
             ...parameter,
-            defaultValue: {
-                ...parameterDefaultValue,
+            value: {
+                ...currentValue,
                 options: updatedOptions,
+                selected: newSelected,
             },
         });
     };
