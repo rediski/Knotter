f1f90ed7fd0dc00c3558ff58bb5b2aad7763002f
Attato <rde8519@mail.ru>
2026-05-03 22:23:14 +0300

    remove static export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-    output: 'export',
-    trailingSlash: true,
-};
+const nextConfig: NextConfig = { output: 'export', trailingSlash: true };
 
 export default nextConfig;
