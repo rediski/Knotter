89a136b13f73bc59286df64a50588c69f076d568
Attato <rde8519@mail.ru>
2026-05-17 21:50:01 +0300

    replace DnD with move buttons in hierarchy

--- app/canvas/components/sidebar/Hierarchy.tsx
+++ app/canvas/components/sidebar/Hierarchy.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { memo, Fragment } from 'react';
+import { memo, Fragment, useCallback } from 'react';
 
 import { EmptyState } from '@/components/UI/EmptyState';
 import { HierarchyItem } from '@/canvas/components/sidebar/HierarchyItem';
@@ -9,29 +9,125 @@ import { useHierarchy } from '@/canvas/components/sidebar/useHierarchy';
 import { useSidebarStore } from '@/canvas/store/useSidebarStore';
 import { useItemsStore } from '@/canvas/store/useItemsStore';
 import { getNodes } from '@/canvas/utils/nodes/getNodes';
+import { moveNodeUp, moveNodeDown } from '@/canvas/utils/nodes/moveNode';
+import { getRangeSelection } from '@/canvas/utils/canvas/getRangeSelection';
+
+import { ArrowBigUp, ArrowBigDown } from 'lucide-react';
 
 export const Hierarchy = memo(function Hierarchy({ panelId }: { panelId?: string }) {
     const items = useItemsStore((state) => state.items);
+    const setItems = useItemsStore((state) => state.setItems);
+    const selectedItemIds = useItemsStore((state) => state.selectedItemIds);
+    const setSelectedItemIds = useItemsStore((state) => state.setSelectedItemIds);
+
     const nodes = getNodes(items);
 
     const filterText = useSidebarStore((state) => (panelId ? state.filterText[panelId] : ''));
     const { filteredNodes, deselect } = useHierarchy(filterText);
 
+    const selectItem = (nodeId: string, ctrlKey: boolean, shiftKey: boolean) => {
+        if (ctrlKey) {
+            const newSet = new Set(selectedItemIds);
+            const wasDeleted = newSet.delete(nodeId);
+
+            if (!wasDeleted) {
+                newSet.add(nodeId);
+            }
+
+            setSelectedItemIds(Array.from(newSet));
+            return;
+        }
+
+        if (shiftKey && selectedItemIds.length > 0) {
+            const lastSelectedId = selectedItemIds[selectedItemIds.length - 1];
+            const rangeSet = getRangeSelection(filteredNodes, nodeId, lastSelectedId);
+
+            if (rangeSet.size > 0) {
+                setSelectedItemIds(Array.from(rangeSet));
+            }
+
+            return;
+        }
+
+        setSelectedItemIds([nodeId]);
+    };
+
+    const selectedNodesList = selectedItemIds;
+    const firstSelectedId = selectedNodesList[0];
+    const lastSelectedId = selectedNodesList[selectedNodesList.length - 1];
+
+    const firstSelectedIndex = firstSelectedId ? filteredNodes.findIndex((node) => node.id === firstSelectedId) : -1;
+    const lastSelectedIndex = lastSelectedId ? filteredNodes.findIndex((node) => node.id === lastSelectedId) : -1;
+
+    const handleMoveUp = useCallback(() => {
+        if (selectedItemIds.length === 0) return;
+        if (firstSelectedIndex <= 0) return;
+
+        let newItems = items;
+
+        for (const nodeId of selectedItemIds) {
+            newItems = moveNodeUp(newItems, nodeId);
+        }
+
+        setItems(newItems);
+    }, [items, setItems, selectedItemIds, firstSelectedIndex]);
+
+    const handleMoveDown = useCallback(() => {
+        if (selectedItemIds.length === 0) return;
+        if (lastSelectedIndex >= filteredNodes.length - 1) return;
+
+        let newItems = items;
+        const reversedIds = [...selectedItemIds].reverse();
+
+        for (const nodeId of reversedIds) {
+            newItems = moveNodeDown(newItems, nodeId);
+        }
+
+        setItems(newItems);
+    }, [items, setItems, selectedItemIds, lastSelectedIndex, filteredNodes.length]);
+
     return (
-        <ul className="flex flex-col gap-1 m-1 pr-1 overflow-y-auto h-full" onClick={deselect}>
+        <div className="flex flex-col h-full">
             {filteredNodes.length !== 0 && (
-                <Fragment>
-                    {filteredNodes.map((filteredNode, index) => (
-                        <HierarchyItem key={filteredNode.id} filteredNode={filteredNode} index={index} />
-                    ))}
-                </Fragment>
+                <div className="flex gap-1 m-1">
+                    <button
+                        onClick={handleMoveUp}
+                        className="flex-1 flex items-center justify-center gap-1 px-2 py-1.75 bg-depth-2 hover:bg-depth-3 active:bg-depth-4 rounded-md border border-depth-3 cursor-pointer"
+                        title="Вверх"
+                    >
+                        <ArrowBigUp size={16} fill="currentColor" />
+                    </button>
+
+                    <button
+                        onClick={handleMoveDown}
+                        className="flex-1 flex items-center justify-center gap-1 px-2 py-1.75 bg-depth-2 hover:bg-depth-3 active:bg-depth-4 rounded-md border border-depth-3 cursor-pointer"
+                        title="Вниз"
+                    >
+                        <ArrowBigDown size={16} fill="currentColor" />
+                    </button>
+                </div>
             )}
 
-            {nodes.length === 0 && <EmptyState message="Создайте элемент, нажав ПКМ по холсту." />}
+            <ul className="flex flex-col gap-1 mx-1 mb-1 overflow-y-auto flex-1" onClick={deselect}>
+                {filteredNodes.length !== 0 && (
+                    <Fragment>
+                        {filteredNodes.map((filteredNode, index) => (
+                            <HierarchyItem
+                                key={filteredNode.id}
+                                filteredNode={filteredNode}
+                                index={index}
+                                selectItem={selectItem}
+                            />
+                        ))}
+                    </Fragment>
+                )}
 
-            {filteredNodes.length === 0 && nodes.length !== 0 && (
-                <EmptyState message={`Не найдено элементов по запросу "${filterText}"`} />
-            )}
-        </ul>
+                {nodes.length === 0 && <EmptyState message="Создайте элемент, нажав ПКМ по холсту." />}
+
+                {filteredNodes.length === 0 && nodes.length !== 0 && (
+                    <EmptyState message={`Не найдено элементов по запросу "${filterText}"`} />
+                )}
+            </ul>
+        </div>
     );
 });
--- app/canvas/components/sidebar/HierarchyItem.tsx
+++ app/canvas/components/sidebar/HierarchyItem.tsx
@@ -1,24 +1,24 @@
 'use client';
 
-import { memo } from 'react';
+import { memo, type MouseEvent } from 'react';
 
 import type { Node } from '@/canvas/_core/_/canvas.types';
 
 import { EditableName } from '@/components/UI/EditableName';
 
 import { useItemsStore } from '@/canvas/store/useItemsStore';
-
-import { useDragAndDrop } from '@/hooks/useDragAndDrop';
 import { useHierarchyItem } from '@/canvas/components/sidebar/useHierarchyItem';
 
 import { Box } from 'lucide-react';
 
-export const HierarchyItem = memo(function HierarchyItem({ filteredNode, index }: { filteredNode: Node; index: number }) {
-    const { handleSelect, handleKeyDown, handleNameChange, handleNodeDoubleClick } = useHierarchyItem(filteredNode);
+interface HierarchyItemProps {
+    filteredNode: Node;
+    index: number;
+    selectItem: (id: string, ctrlKey: boolean, shiftKey: boolean) => void;
+}
 
-    const { dragRef, dropRef, isDragOver, dragPosition } = useDragAndDrop<HTMLDivElement, HTMLLIElement>({
-        itemId: filteredNode.id,
-    });
+export const HierarchyItem = memo(function HierarchyItem({ filteredNode, index, selectItem }: HierarchyItemProps) {
+    const { handleKeyDown, handleNameChange, handleNodeDoubleClick } = useHierarchyItem(filteredNode);
 
     const selectedItemIds = useItemsStore((state) => state.selectedItemIds);
     const isSelected = selectedItemIds.includes(filteredNode.id);
@@ -27,39 +27,30 @@ export const HierarchyItem = memo(function HierarchyItem({ filteredNode, index }
 
     return (
         <li
-            ref={dropRef}
-            className="relative select-none"
-            onClick={handleSelect}
+            className="relative select-none cursor-pointer"
+            onClick={(e: MouseEvent) => selectItem(filteredNode.id, e.ctrlKey, e.shiftKey)}
             onKeyDown={handleKeyDown}
             onDoubleClick={handleNodeDoubleClick}
         >
-            {isDragOver && (
-                <div
-                    className={`absolute left-0 right-0 h-0.5 bg-bg-accent ${dragPosition === 'top' ? 'top-0' : 'bottom-0'}`}
-                />
-            )}
+            <div
+                className={`
+                    w-full px-3 h-9 rounded-md outline-none tabular-nums flex items-center
+                    ${
+                        isSelected
+                            ? 'bg-bg-accent/10 border border-bg-accent/10 focus-visible:bg-bg-accent/15'
+                            : 'bg-depth-2 hover:bg-depth-3 border border-depth-3 focus-visible:bg-depth-3'
+                    }
+                `}
+            >
+                <div className="flex items-center gap-2 flex-1">
+                    <Box size={16} />
 
-            <div ref={dragRef} draggable>
-                <button
-                    className={`
-                        w-full px-3 h-9 rounded-md outline-none tabular-nums cursor-grab active:cursor-grabbing
-                        ${
-                            isSelected
-                                ? 'bg-bg-accent/10 border border-bg-accent/10 focus-visible:bg-bg-accent/15'
-                                : 'bg-depth-2 hover:bg-depth-3 border border-depth-3 focus-visible:bg-depth-3'
-                        }
-                    `}
-                >
-                    <div className="flex items-center gap-2 w-full pointer-events-none">
-                        <Box size={16} />
+                    <div className={`border-l h-5 ${isSelected ? 'border-bg-accent/10' : 'border-depth-4'}`} />
 
-                        <div className={`border-l h-5 ${isSelected ? 'border-bg-accent/10' : 'border-depth-4'}`} />
+                    <EditableName name={filteredNode.name} isSelected={isSelected} onChange={handleNameChange} />
 
-                        <EditableName name={filteredNode.name} isSelected={isSelected} onChange={handleNameChange} />
-
-                        <span className="ml-auto text-xs text-gray tabular-nums">#{orderNumber}</span>
-                    </div>
-                </button>
+                    <span className="ml-auto text-xs text-gray tabular-nums">#{orderNumber}</span>
+                </div>
             </div>
         </li>
     );
