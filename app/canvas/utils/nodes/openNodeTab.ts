258c207ecafb700f70f3a9274e64a01029a9a41d
Attato <rde8519@mail.ru>
2026-05-23 13:01:26 +0300

    add activeSceneId to node tab navigation

--- app/canvas/utils/nodes/openNodeTab.ts
+++ app/canvas/utils/nodes/openNodeTab.ts
@@ -1,13 +1,18 @@
 import { useCanvasStore } from '@/canvas/store/useCanvasStore';
+import { useItemsStore } from '@/canvas/store/useItemsStore';
+
 import type { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
 export const openNodeTab = (nodeId: string, router: AppRouterInstance) => {
     const openedTabIds = useCanvasStore.getState().openedTabIds;
     const setOpenedTabIds = useCanvasStore.getState().setOpenedTabIds;
+    const activeSceneId = useItemsStore.getState().activeSceneId;
 
     if (!openedTabIds.includes(nodeId)) {
         setOpenedTabIds([...openedTabIds, nodeId]);
     }
 
-    router.push(`/canvas/${nodeId}`);
+    if (activeSceneId !== null) {
+        router.push(`/canvas/${activeSceneId}/${nodeId}`);
+    }
 };
