abefbe29211c50507263fd7c4af9b464c3c69dd0
Attato <rde8519@mail.ru>
2026-05-24 22:23:45 +0300

    add openWorkspace function

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,33 @@
 'use client';
 
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-import { ThemeToggle } from '@/components/ThemeToggle';
+import { ThemeToggle } from '@/components/UI/ThemeToggle';
+import { createScene } from '@/utils/scene/createScene';
+import { useItemsStore } from '@/store/useItemsStore';
 
 export default function Header() {
+    const router = useRouter();
+    const scenes = useItemsStore((state) => state.scenes);
+
+    const openWorkspace = async () => {
+        const sceneIds = Object.keys(scenes);
+
+        if (sceneIds.length === 0) {
+            const sceneId = await createScene();
+
+            if (sceneId) {
+                router.push(`/${sceneId}`);
+            }
+
+            return;
+        }
+
+        const firstSceneId = sceneIds[0];
+        router.push(`/${firstSceneId}`);
+    };
+
     return (
         <header className="sticky top-4 w-full z-50">
             <div className="container flex justify-between items-center bg-depth-1 border border-depth-3 px-6 py-1 rounded-md mx-auto">
@@ -19,12 +42,12 @@ export default function Header() {
 
                     <div className="h-6 border-l border-depth-3" />
 
-                    <Link
-                        href="/canvas"
-                        className="flex items-center w-fit text-sm px-3 h-8 bg-bg-accent/10 hover:bg-bg-accent/15 border border-bg-accent/15 hover:border-bg-accent/20 transition-colors text-text-accent rounded-lg select-none"
+                    <button
+                        onClick={openWorkspace}
+                        className="flex items-center w-fit text-sm px-3 h-8 bg-bg-accent/10 hover:bg-bg-accent/15 border border-bg-accent/15 hover:border-bg-accent/20 transition-colors text-text-accent rounded-lg select-none cursor-pointer"
                     >
                         Open Workspace
-                    </Link>
+                    </button>
                 </div>
             </div>
 
