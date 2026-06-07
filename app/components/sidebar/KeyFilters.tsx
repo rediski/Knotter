28fcdad5a2bbdcfd7955276bde6befa5221805c7
Attato <rde8519@mail.ru>
2026-06-07 20:18:33 +0300

    style fix

--- app/components/sidebar/KeyFilters.tsx
+++ app/components/sidebar/KeyFilters.tsx
@@ -70,11 +70,11 @@ export function KeyFilters<T extends AnyObject = AnyObject>({ data, onFilterChan
     }, [filteredData]);
 
     return (
-        <div className="flex flex-col gap-1 max-h-64 select-none top-0 sticky">
+        <div className="flex flex-col gap-1 select-none top-0 sticky">
             {allKeys.map((key) => (
                 <label
                     key={key}
-                    className="flex items-center gap-2 text-md cursor-pointer bg-depth-2 hover:bg-depth-3 border border-depth-3 flex-1 min-w-64 px-3 py-1 rounded-md"
+                    className="flex items-center gap-2 text-md cursor-pointer bg-depth-2 hover:bg-depth-3 border border-depth-3 min-w-64 px-3 py-1 h-fit rounded-md"
                 >
                     <Checkbox
                         checked={selectedKeys.has(key)}
