5187e934a5c29475796577790ae9e557bed2334e
Attato <rde8519@mail.ru>
2026-07-11 18:57:36 +0300

    add UPDATE_ITEMS

--- app/_core/_/history.types.ts
+++ app/_core/_/history.types.ts
@@ -6,4 +6,5 @@ export type CanvasAction =
     | { type: 'ADD_ITEMS'; items: CanvasItem[] }
     | { type: 'DELETE_ITEMS'; ids: string[] }
     | { type: 'PASTE_ITEMS'; items: CanvasItem[] }
-    | { type: 'MOVE_ITEMS'; items: CanvasItem[] };
+    | { type: 'MOVE_ITEMS'; items: CanvasItem[] }
+    | { type: 'UPDATE_ITEMS'; items: CanvasItem[] };
