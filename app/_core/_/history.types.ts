4a7cf21c5d60c32a309244f3ad49ea25d74e6958
Attato <rde8519@mail.ru>
2026-07-20 22:25:54 +0300

    change DELETE_ITEMS payload from ids to items

--- app/_core/_/history.types.ts
+++ app/_core/_/history.types.ts
@@ -4,6 +4,6 @@ export const MAX_HISTORY_SIZE = 100;
 
 export type CanvasAction =
     | { type: 'ADD_ITEMS'; items: CanvasItem[] }
-    | { type: 'DELETE_ITEMS'; ids: string[] }
+    | { type: 'DELETE_ITEMS'; items: CanvasItem[] }
     | { type: 'PASTE_ITEMS'; items: CanvasItem[] }
     | { type: 'CHANGE_ITEMS'; items: CanvasItem[] };
