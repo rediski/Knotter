bfb0ba768e80a999d302ca9895531492c49b302e
Attato <rde8519@mail.ru>
2026-07-30 11:12:05 +0300

    formatTimestamp init

---
+++ app/utils/date/formatTimestamp.ts
@@ -0,0 +1,7 @@
+export const formatTimestamp = (timestamp: number): string => {
+    const date = new Date(timestamp);
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${hours}:${minutes}`;
+};
