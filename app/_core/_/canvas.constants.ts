5685a64a25ad060888863bd819c34e5d9fd8c3a5
Attato <rde8519@mail.ru>
2026-07-30 15:52:25 +0300

    delete MAX_UNDO_STEPS

--- app/_core/_/canvas.constants.ts
+++ app/_core/_/canvas.constants.ts
@@ -3,8 +3,6 @@ export const MAX_ZOOM = 10;
 export const INITIAL_ZOOM = 1;
 export const ZOOM_FACTOR = 1.1;
 
-export const MAX_UNDO_STEPS = 50;
-
 export const NODE_SIZE = 40;
 export const NODE_MOVE_MIN_STEP = 1;
 export const NODE_MOVE_MAX_STEP = 50;
