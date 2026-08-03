e488dc9935ba82b30257f105aca38f91392eef65
Attato <rde8519@mail.ru>
2026-08-03 11:40:56 +0300

    implement TitleUpdater component

--- app/[sceneId]/layout.tsx
+++ app/[sceneId]/layout.tsx
@@ -1,23 +1,19 @@
 import type { ReactNode } from 'react';
-import { Metadata } from 'next';
 import Link from 'next/link';
 
-import { Sidebar } from '@/components/sidebar/Sidebar';
 import { ThemeToggle } from '@/components/UI/ThemeToggle';
+import { TitleUpdater } from '@/components/scene/TitleUpdater';
 import { Breadcrumbs } from '@/components/scene/Breadcrumbs';
 import { SceneList } from '@/components/scene/SceneList';
+import { Sidebar } from '@/components/sidebar/Sidebar';
 
 import { Home } from 'lucide-react';
-
-export const metadata: Metadata = {
-    title: 'Холст',
-    description:
-        'Knotter - это нодовый редактор для работы с данными, распространяемый под лицензией GPL-3.0, которая гарантирует, что любые производные работы и модификации останутся столь же свободными и открытыми.',
-};
 
 export default function CanvasLayout({ children }: { children: ReactNode }) {
     return (
         <div className="flex w-full h-screen bg-background p-1 overflow-x-hidden" translate="no">
+            <TitleUpdater />
+
             <div className="flex flex-col flex-1 gap-1">
                 <div className="fixed flex items-center gap-1 z-20 bg-depth-1 p-1 m-1 rounded-md border border-depth-3 shadow-xs">
                     <Link
