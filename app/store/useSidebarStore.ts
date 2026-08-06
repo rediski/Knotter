c918654a7fbd8e7ff6ed6fa98843213e1caff466
Attato <rde8519@mail.ru>
2026-08-06 21:57:26 +0300

    change viewMode from items to scenes

--- app/store/useSidebarStore.ts
+++ app/store/useSidebarStore.ts
@@ -3,7 +3,7 @@ import { persist } from 'zustand/middleware';
 import type { SidebarPanel } from '@/_core/_/sidebarPanel';
 import { v4 as uuid } from 'uuid';
 
-export type DataViewMode = 'items' | 'parameters';
+export type DataViewMode = 'scenes' | 'parameters';
 
 export interface SidebarState {
     sidebarPanels: SidebarPanel[];
@@ -70,7 +70,7 @@ export const useSidebarStore = create<SidebarState>()(
             setDataViewMode: (dataViewMode) => set({ dataViewMode }),
             toggleDataViewMode: () =>
                 set((state) => ({
-                    dataViewMode: state.dataViewMode === 'parameters' ? 'items' : 'parameters',
+                    dataViewMode: state.dataViewMode === 'parameters' ? 'scenes' : 'parameters',
                 })),
         }),
         {
