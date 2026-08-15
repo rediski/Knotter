803c18133a3d9c09fbb6651a937e3ac63f80c929
Attato <rde8519@mail.ru>
2026-08-15 22:16:20 +0300

    remove createParameterInStructure and add "value" field to createParameter

--- app/utils/parameters/createParameter.ts
+++ app/utils/parameters/createParameter.ts
@@ -1,54 +1,20 @@
-import type { ParameterType, Parameter } from '@/_core/_/parameter';
-import { parameterInitialValue } from '@/utils/parameters/parameterInitialValue';
-
+import type { Parameter } from '@/_core/_/parameter';
 import { useItemsStore } from '@/store/useItemsStore';
-import { isStructure } from '@/_core/_/parameter.type-guards';
-
 import { v4 as uuid } from 'uuid';
 
-export const createParameter = (name: string, type: ParameterType): Parameter => {
+export const createParameter = (name: string): Parameter => {
     const itemsState = useItemsStore.getState();
 
     const newParameter: Parameter = {
         id: uuid(),
         name,
-        type,
-        defaultValue: parameterInitialValue(type),
+        type: null,
+        value: null,
+        defaultValue: null,
         parentId: null,
-    } as Parameter;
+    };
 
     itemsState.setParameters([...itemsState.parameters, newParameter]);
-
-    return newParameter;
-};
-
-export const createParameterInStructure = (
-    name: string,
-    type: ParameterType,
-    parentStructureId: string,
-): Parameter | null => {
-    const itemsState = useItemsStore.getState();
-
-    const newParameter: Parameter = {
-        id: uuid(),
-        name,
-        type,
-        defaultValue: parameterInitialValue(type),
-        parentId: parentStructureId,
-    } as Parameter;
-
-    const updatedParameters = itemsState.parameters.map((parameter) => {
-        if (parameter.id === parentStructureId && isStructure(parameter)) {
-            return {
-                ...parameter,
-                defaultValue: [...parameter.defaultValue, newParameter.id],
-            };
-        }
-
-        return parameter;
-    });
-
-    itemsState.setParameters([...updatedParameters, newParameter]);
 
     return newParameter;
 };
