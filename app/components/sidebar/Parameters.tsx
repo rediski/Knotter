67cca3296bd87c86f5dbfd00c9d994e9c75e6deb
Attato <rde8519@mail.ru>
2026-08-16 22:17:29 +0300

    add parameters management with dnd

--- app/components/sidebar/Parameters.tsx
+++ app/components/sidebar/Parameters.tsx
@@ -1,66 +1,41 @@
+'use client';
+
 import { useParams } from 'next/navigation';
 
 import { EmptyState } from '@/components/UI/EmptyState';
 
-import { ParameterItem } from '@/components/parameters/ParameterItem';
-import { useNodeContent } from '@/components/parameters/useNodeContent';
+import { ParameterItem } from '@/components/sidebar/ParameterItem';
+import { useParameters } from '@/components/sidebar/useParameters';
 
 import { useItemsStore } from '@/store/useItemsStore';
 
 import { hasParameterInNode } from '@/utils/nodes/hasParameterInNode';
-import { addSelectedParametersToNode } from '@/utils/nodes/addSelectedParametersToNode';
 import { createParameter } from '@/utils/parameters/createParameter';
 
-import { Plus, X } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import { generateUniqueName } from '@/utils/items/generateUniqueName';
 
-export const Paramters = () => {
+export const Parameters = () => {
     const params = useParams();
     const nodeId = params.nodeId as string;
 
     const {
         filteredParameters,
         selectedParameters,
-
         selectParameters,
-        deleteSelectedParameters,
-
+        deselect,
+        visibleSelectedCount,
         listRef,
         handleDragStart,
         handleDragOver,
         handleDrop,
         handleDragEnd,
-    } = useNodeContent();
+    } = useParameters();
 
     const parameters = useItemsStore((state) => state.parameters);
-
-    const selectedIds = Array.from(selectedParameters);
-    const selectedParametersList = parameters.filter((parameter) => selectedParameters.has(parameter.id));
-
-    const hasSelectedParameters = selectedIds.length > 0;
-    const hasNotAddedParameters = selectedIds.some((id) => !hasParameterInNode(id, nodeId));
-    const hasRootParameters = selectedParametersList.some((parameter) => parameter.parentId === null);
-
-    const canAddSelected = hasSelectedParameters && hasNotAddedParameters && hasRootParameters;
-    const canDeleteSelected = hasSelectedParameters && hasNotAddedParameters;
-
-    const actionButtons = [
-        {
-            onClick: () => addSelectedParametersToNode(nodeId),
-            icon: Plus,
-            iconProps: { size: 16 },
-            disabled: !canAddSelected,
-        },
-        {
-            onClick: deleteSelectedParameters,
-            icon: X,
-            iconProps: { size: 16 },
-            disabled: !canDeleteSelected,
-        },
-    ];
+    const currentNodeId = useItemsStore((state) => state.currentNodeId);
 
     const baseName = 'Параметр';
-
     const name = generateUniqueName(
         baseName,
         parameters.map((parameter) => parameter.name),
@@ -70,63 +45,46 @@ export const Paramters = () => {
         <div className="flex flex-col gap-1 flex-1 overflow-y-auto max-h-[calc(100vh-8px-32px-4px)]">
             <button
                 onClick={() => createParameter(name)}
-                className="flex items-center justify-center gap-2 h-8 rounded-md cursor-pointer bg-depth-2 hover:bg-depth-3 active:bg-depth-4 text-foreground border border-depth-3 text-sm"
+                className="flex items-center justify-center gap-2 m-1 mb-0 h-8 rounded-md cursor-pointer bg-depth-2 hover:bg-depth-3 active:bg-depth-4 text-foreground border border-depth-3 text-sm"
             >
                 <Plus size={16} />
                 <span>Создать</span>
             </button>
 
-            <div className="flex flex-col flex-1 w-full overflow-y-auto bg-depth-1 border border-depth-3 rounded-md">
-                <div className="flex gap-1 sticky top-0 bg-depth-1 z-20 p-1 border-b border-depth-3">
-                    {actionButtons.map((button, index) => (
-                        <button
-                            key={index}
-                            onClick={button.onClick}
-                            disabled={button.disabled}
-                            className={`
-                                flex flex-1 items-center justify-center w-8 h-8 p-1 bg-depth-2 hover:bg-depth-3 rounded-md border border-depth-3 disabled:opacity-30 
-                                ${button.disabled ? 'cursor-not-allowed' : 'cursor-pointer'}
-                            `}
-                        >
-                            <button.icon {...button.iconProps} />
-                        </button>
-                    ))}
-                </div>
+            <ul
+                className="flex flex-col gap-1 p-1 list-none relative"
+                ref={listRef}
+                onDragOver={handleDragOver}
+                onDrop={handleDrop}
+                onDragEnd={handleDragEnd}
+                onClick={deselect}
+            >
+                {filteredParameters.length > 0 ? (
+                    <>
+                        {visibleSelectedCount > 1 && (
+                            <div className="text-xs text-text-accent px-3 py-1 bg-bg-accent/5 rounded-md truncate">
+                                Выбранных параметров: {visibleSelectedCount}
+                            </div>
+                        )}
 
-                <ul
-                    className="flex flex-col gap-1 p-1 list-none relative"
-                    ref={listRef}
-                    onDragOver={handleDragOver}
-                    onDrop={handleDrop}
-                    onDragEnd={handleDragEnd}
-                >
-                    {filteredParameters.length > 0 ? (
-                        <>
-                            {filteredParameters.map((parameter) => (
-                                <li
-                                    key={parameter.id}
-                                    className="list-none"
-                                    draggable
-                                    onDragStart={(e) => handleDragStart(e, parameter.id)}
-                                >
-                                    <ParameterItem
-                                        parameter={parameter}
-                                        selectedIds={selectedParameters}
-                                        onSelect={selectParameters}
-                                        hasParameterInNode={hasParameterInNode(parameter.id, nodeId)}
-                                    />
-                                </li>
-                            ))}
-                        </>
-                    ) : (
-                        <EmptyState
-                            message={
-                                parameters.length === 0 ? 'Параметры не найдены' : 'Параметры с таким именем не найдены'
-                            }
-                        />
-                    )}
-                </ul>
-            </div>
+                        {filteredParameters.map((parameter, index) => (
+                            <ParameterItem
+                                key={parameter.id}
+                                parameter={parameter}
+                                selectedIds={selectedParameters}
+                                onSelect={selectParameters}
+                                hasParameterInNode={hasParameterInNode(parameter.id, nodeId)}
+                                currentNodeId={currentNodeId}
+                                handleDragStart={handleDragStart}
+                            />
+                        ))}
+                    </>
+                ) : (
+                    <EmptyState
+                        message={parameters.length === 0 ? 'Параметры не найдены' : 'Параметры с таким именем не найдены'}
+                    />
+                )}
+            </ul>
         </div>
     );
 };
