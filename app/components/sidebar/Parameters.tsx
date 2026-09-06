09094a1d49872afbbc95c7501520d7dd9bbd24b2
Attato <rde8519@mail.ru>
2026-09-06 23:33:31 +0300

    remove useParameters hook and simplify component

--- app/components/sidebar/Parameters.tsx
+++ app/components/sidebar/Parameters.tsx
@@ -3,7 +3,6 @@
 import { EmptyState } from '@/components/UI/EmptyState';
 
 import { ParameterItem } from '@/components/sidebar/ParameterItem';
-import { useParameters } from '@/components/sidebar/useParameters';
 
 import { useItemsStore } from '@/store/useItemsStore';
 
@@ -12,22 +11,26 @@ import { generateUniqueName } from '@/utils/items/generateUniqueName';
 import { createParameter } from '@/utils/parameters/createParameter';
 
 import { Plus } from 'lucide-react';
+import { getVisibleSelectedParametersCount } from '@/utils/parameters/getVisibleSelectedParametersCount';
+
+import { useDragAndDrop } from '@/hooks/useDragAndDrop';
+import { Parameter } from '@/_core/_/parameter';
+
+import { handleDragReorderParameters } from '@/utils/parameters/handleDragReorderParameters';
 
 export const Parameters = () => {
-    const {
-        filteredParameters,
-        selectedParameters,
-        selectParameters,
-        deselect,
-        visibleSelectedCount,
-        listRef,
-        handleDragStart,
-        handleDragOver,
-        handleDrop,
-        handleDragEnd,
-    } = useParameters();
-
     const parameters = useItemsStore((state) => state.parameters);
+    const setSelectedParameterIds = useItemsStore((state) => state.setSelectedParameterIds);
+
+    const selectedParameterIds = useItemsStore((state) => state.selectedParameterIds);
+
+    const { listRef, handleDragStart, handleDragOver, handleDrop, handleDragEnd } = useDragAndDrop<Parameter>({
+        items: parameters,
+        selectedIds: selectedParameterIds,
+        onSelect: (ids) => setSelectedParameterIds(ids),
+        onReorder: handleDragReorderParameters,
+        itemSelector: 'li',
+    });
 
     const baseName = 'Параметр';
 
@@ -35,6 +38,8 @@ export const Parameters = () => {
         baseName,
         parameters.map((parameter) => parameter.name),
     );
+
+    const visibleSelectedParametersCount = getVisibleSelectedParametersCount(parameters);
 
     return (
         <div className="flex flex-col overflow-y-auto max-h-[calc(100vh-8px-32px-4px)] mt-1">
@@ -54,24 +59,18 @@ export const Parameters = () => {
                 onDragOver={handleDragOver}
                 onDrop={handleDrop}
                 onDragEnd={handleDragEnd}
-                onClick={deselect}
+                onClick={() => setSelectedParameterIds([])}
             >
-                {filteredParameters.length > 0 ? (
+                {parameters.length > 0 ? (
                     <>
-                        {visibleSelectedCount > 1 && (
+                        {visibleSelectedParametersCount > 1 && (
                             <div className="text-xs text-text-accent px-3 py-1 bg-bg-accent rounded-md truncate">
-                                Выбранных параметров: {visibleSelectedCount}
+                                Выбранных параметров: {visibleSelectedParametersCount}
                             </div>
                         )}
 
-                        {filteredParameters.map((parameter) => (
-                            <ParameterItem
-                                key={parameter.id}
-                                parameter={parameter}
-                                selectedIds={selectedParameters}
-                                onSelect={selectParameters}
-                                handleDragStart={handleDragStart}
-                            />
+                        {parameters.map((parameter) => (
+                            <ParameterItem key={parameter.id} parameter={parameter} handleDragStart={handleDragStart} />
                         ))}
                     </>
                 ) : (
